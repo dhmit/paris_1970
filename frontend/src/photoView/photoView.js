c3758b889ab0edf4ceb5dd646bd9e2e83c4147ea
Jason Lin <jasonlin43212@gmail.com>
2020-07-27 12:50:00 -0400

    added a previous and next link

--- frontend/src/photoView/photoView.js
+++ frontend/src/photoView/photoView.js
@@ -8,6 +8,7 @@ export class PhotoView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            loading: true,
             photo_data: null,
         };
     }
@@ -16,17 +17,29 @@ export class PhotoView extends React.Component {
         try {
             const photoId = this.props.id;
             const response = await fetch(`/api/photo/${photoId}/`);
-            const photo_data = await response.json();
-            this.setState({ photo_data });
+            if (!response.ok) {
+                this.setState({ loading: false });
+            } else {
+                const photo_data = await response.json();
+                this.setState({ photo_data, loading: false });
+            }
         } catch (e) {
             console.log(e);
         }
     }
 
     render() {
-        if (!this.state.photo_data) {
+        if (this.state.loading) {
             return (<>
                 Loading!
+            </>);
+        }
+        if (!this.state.photo_data) {
+            return (<>
+                Photo with id {this.props.id} is not in database.
+                <a href={`/photos/${this.props.id - 1}/`}>Prev</a>
+                <br/>
+                <a href={`/photos/${this.props.id + 1}/`}>Next</a>
             </>);
         }
         const src = this.state.photo_data.src;
@@ -35,10 +48,14 @@ export class PhotoView extends React.Component {
 
         return (<>
             <Navbar />
-            <div style={{ height: '75px' }}>-</div>
-            <p>Hello world.</p>
-            <img width={500} height={500} src={src} alt={alt}/>
-            <p>{title}</p>
+            <div style={{ padding: '100px 50px'}}>
+                <h2>{title}</h2>
+                <img width={500} height={500} src={src} alt={alt}/>
+                <br/>
+                <a href={`/photos/${this.props.id - 1}/`}>Prev</a>
+                <br/>
+                <a href={`/photos/${this.props.id + 1}/`}>Next</a>
+            </div>
             <Footer />
         </>);
     }
