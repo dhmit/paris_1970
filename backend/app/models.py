e0fca56ac330431f214a49954c0e207716094d22
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2020-07-29 17:42:30 -0400

    Fixed merge conflicts for: merge homepage_list_view into variable_photos

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,25 +10,12 @@ class Photo(models.Model):
     front_src = models.CharField(max_length=252)
     back_src = models.CharField(max_length=252)
     alt = models.CharField(max_length=252)
-    map_square_obj = models.ForeignKey('MapSquare', on_delete=models.SET_NULL, null=True)
-    photographer_obj = models.ForeignKey('Photographer', on_delete=models.SET_NULL, null=True)
 
 
 class MapSquare(models.Model):
     name = models.CharField(max_length=252)
     photo_ids = models.ManyToManyField(
-        Photo,
-        blank=True,
-    )
-    boundaries = models.CharField(max_length=252)
-
-
-class Photographer(models.Model):
-    name = models.CharField(max_length=252)
-    type = models.CharField(max_length=252)
-    sentiment = models.CharField(max_length=252)
-    photo_ids = models.ManyToManyField(
         'Photo',
         blank=True,
     )
-    map_square_obj = models.ForeignKey('MapSquare', on_delete=models.SET_NULL, null=True)
+    boundaries = models.CharField(max_length=252)
--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -4,7 +4,7 @@ in ways that can be transported across the backend/frontend divide, or
 allow the frontend to suggest changes to the backend/database.
 """
 from rest_framework import serializers
-from .models import Photo, MapSquare
+from .models import Photo, MapSquare, Photographer
 
 
 class PhotoSerializer(serializers.ModelSerializer):
@@ -42,16 +42,6 @@ class MapSquareSerializer(serializers.ModelSerializer):
 
 
 class PhotographerSerializer(serializers.ModelSerializer):
-
-    photos = serializers.SerializerMethodField()
-    map_square = serializers.SerializerMethodField()
-
-    def get_photos(self, instance):
-        return PhotoSerializer(instance.photo_ids, many=True).data
-
-    def get_map_square(self, instance):
-        return MapSquareSerializer(instance.map_square_obj).data
-
     class Meta:
         model = Photographer
-        fields = ['id', 'name', 'type', 'sentiment', 'photos', 'map_square']
+        fields = ['id', 'name', 'type', 'sentiment', 'photo_ids', 'map_square']
