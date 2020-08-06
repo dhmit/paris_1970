40d5da29b4b9dce9044822e92bbd08e0f0f5d88d
Michelle He <mjhe@mit.edu>
2020-08-06 10:03:53 -0700

    Updated photoview
    made a basic photoview based on wireframe

--- frontend/src/photoView/photoView.js
+++ frontend/src/photoView/photoView.js
@@ -10,6 +10,7 @@ export class PhotoView extends React.Component {
         this.state = {
             loading: true,
             photo_data: null,
+            display_side: 'front',
         };
     }
 
@@ -28,35 +29,105 @@ export class PhotoView extends React.Component {
         }
     }
 
-    render() {
-        if (this.state.loading) {
-            return (<>
-                Loading!
-            </>);
-        }
-        if (!this.state.photo_data) {
-            return (<>
-                Photo with id {window.location.pathname.split('/')[2]} is not in database.
-            </>);
-        }
-        const {
-            title,
-            alt,
-            front_src,
-            back_src,
-        } = this.state.photo_data;
+     flipPhoto = (e) => {
+         if (this.state.display_side === 'front') {
+             console.log('time for a change...');
+             this.setState({
+                 display_side: 'back',
+             });
+         } else {
+             this.setState({
+                 display_side: 'front',
+             });
+         }
+     };
 
-        return (<>
-            <Navbar />
-            <div style={{ padding: '100px 50px' }}>
-                <h2>{title}</h2>
-                <img width={500} height={500} src={front_src} alt={alt}/>
-                <img width={500} height={500} src={back_src} alt={alt}/>
-                <br/>
-            </div>
-            <Footer />
-        </>);
-    }
+     render() {
+         if (this.state.loading) {
+             return (<>
+                Loading!
+             </>);
+         }
+         if (!this.state.photo_data) {
+             return (<>
+                Photo with id {window.location.pathname.split('/')[2]} is not in database.
+             </>);
+         }
+         const {
+             title,
+             alt,
+             front_src,
+             back_src,
+         } = this.state.photo_data;
+
+         let src = front_src;
+         if (this.state.display_side === 'back') {
+             src = back_src;
+         }
+
+         return (<>
+             <Navbar />
+             <div>
+                 <div className='imageView' style={{ padding: '100px 50px', float: 'left' }}>
+                     <h2>Photo Title: {title}</h2>
+                     <img width={500} height={500} src={src} alt={alt}/>
+                     <br/>
+                     <button onClick={(e) => this.flipPhoto(e)}> Flip photo </button>
+                 </div>
+                 <div className='imageInfo'style={{ padding: '100px 50px', float: 'left', marginTop: 50 }}>
+                     <div className='attributeTitle' style={{ fontWeight: 'bold' }}>
+                        Photographer:
+                         <div className='attribute'style={{ fontWeight: 'normal' }}>
+                            None
+                         </div>
+                     </div>
+                     <div className='attributeTitle' style={{ fontWeight: 'bold' }}>
+                        Categories
+                         <div className='attribute'style={{ fontWeight: 'normal' }}>
+                            None
+                         </div>
+                     </div>
+                     <div className='attributeTitle' style={{ fontWeight: 'bold' }}>
+                        Whitespace
+                         <div className='attribute'style={{ fontWeight: 'normal' }}>
+                            None
+                         </div>
+                     </div>
+                     <div className='attributeTitle' style={{ fontWeight: 'bold' }}>
+                        Sentiment analysis:
+                         <div className='attribute'style={{ fontWeight: 'normal' }}>
+                            None
+                         </div>
+                     </div>
+                     <div className='attributeTitle' style={{ fontWeight: 'bold' }}>
+                        People detected:
+                         <div className='attribute'style={{ fontWeight: 'normal' }}>
+                            None
+                         </div>
+                     </div>
+                     <div className='attributeTitle' style={{ fontWeight: 'bold' }}>
+                        Text detected:
+                         <div className='attribute'style={{ fontWeight: 'normal' }}>
+                            None
+                         </div>
+                     </div>
+                     <div className='attributeTitle' style={{ fontWeight: 'bold' }}>
+                        Objects detected:
+                         <div className='attribute'style={{ fontWeight: 'normal' }}>
+                            None
+                         </div>
+                     </div>
+                     <div className='attributeTitle' style={{ fontWeight: 'bold' }}>
+                        Map Square Information:
+                         <div className='attribute'style={{ fontWeight: 'normal' }}>
+                            None
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <Footer />
+         </>);
+     }
 }
 PhotoView.propTypes = {
     id: PropTypes.number,
