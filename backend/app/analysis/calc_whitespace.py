b7292397f1109e90c8d875df6740b7f55f2c8800
Jason Lin <jasonlin43212@gmail.com>
2020-08-17 14:31:20 -0400

    made the pickling work with http request error and made it write to pickle more often

--- backend/app/analysis/calc_whitespace.py
+++ backend/app/analysis/calc_whitespace.py
@@ -17,50 +17,47 @@ from app.models import Photo
 
 from .tests import AnalysisTestBase
 
+MODEL = Photo
 
-def analysis() -> dict:
+
+def analysis(photo) -> dict:
     """
     Calculates the whitespace for all sides of a photo
     """
-    result = {}
+    photo_srcs = {
+        'front_src': photo.front_src,
+        'back_src': photo.back_src,
+        'binder_src': photo.binder_src
+    }
+    results = {}
     number_of_photos = len(Photo.objects.all())
-    for photo in Photo.objects.all():
-        photo_srcs = {
-            'front_src': photo.front_src,
-            'back_src': photo.back_src,
-            'binder_src': photo.binder_src
-        }
-        new_attributes = {}
-        for side in ['front', 'back', 'binder']:
-            try:
-                url = photo_srcs[side + '_src']
+    for side in ['front', 'back', 'binder']:
+        try:
+            url = photo_srcs[side + '_src']
 
-                # Get image, will raise ValueError if src url is ''
-                # will raise FileNotFound error if src is just a filename rather than a url
-                image = io.imread(url)
+            # Get image, will raise ValueError if src url is ''
+            # will raise FileNotFound error if src is just a filename rather than a url
+            image = io.imread(url)
 
-                gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                normalized_gray_image = gray_image / np.max(gray_image)
-                number_of_pixels = (normalized_gray_image > .6).sum()
-                white_space_ratio = number_of_pixels / gray_image.size
-                new_attributes[f'white_space_ratio_{side}'] = white_space_ratio
-                print(f'Successfully calculated whitespace ratio for photo {photo.id} {side}')
-            except (ValueError, FileNotFoundError):
-                pass
-            except (HTTPError, RemoteDisconnected):
-                print(dedent(f'''
-                    *** Right now, the analysis breaks after too many http requests, so it may not
-                    calculate whitespace for all the photos, even the first time. If it stops
-                    working, you will have to wait a while before it is successfully able to make
-                    requests again. ***
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            normalized_gray_image = gray_image / np.max(gray_image)
+            number_of_pixels = (normalized_gray_image > .6).sum()
+            white_space_ratio = number_of_pixels / gray_image.size
+            results[f'white_space_ratio_{side}'] = white_space_ratio
+            print(f'Successfully calculated whitespace ratio for photo {photo.id} {side}')
+        except (ValueError, FileNotFoundError):
+            pass
+        except (HTTPError, RemoteDisconnected):
+            raise Exception(dedent(f'''
+                *** Right now, the analysis breaks after too many http requests, so it may not
+                calculate whitespace for all the photos, even the first time. If it stops
+                working, you will have to wait a while before it is successfully able to make
+                requests again. ***
 
-                    Successfully calculated whitespace ratio for {photo.id-1}/{number_of_photos}
-                    photos.
-                '''))
-                return result
-        result[photo.id] = new_attributes
-    print('Successfully calculated whitespace ratio for all photos.')
-    return result
+                Successfully calculated whitespace ratio for {photo.id-1}/{number_of_photos}
+                photos.
+            '''))
+    return results
 
 
 class TestAnalysis(AnalysisTestBase):
--- backend/app/management/commands/runanalysis.py
+++ backend/app/management/commands/runanalysis.py
@@ -47,10 +47,15 @@ class Command(BaseCommand):
             model = getattr(analysis_module, 'MODEL')
 
             for model_instance in model.objects.all():
+                print(model_instance.id)
                 instance_identifier = f'{model.__name__}_{model_instance.id}'
 
                 if rerun or instance_identifier not in past_results:
-                    analysis_results = analysis_func(model_instance)
+                    try:
+                        analysis_results = analysis_func(model_instance)
+                    except Exception as e:
+                        print(e)
+                        return
                 else:
                     analysis_results = past_results[instance_identifier]
 
@@ -61,6 +66,7 @@ class Command(BaseCommand):
                 # Store the result
                 past_results[f'{model.__name__}_{model_instance.id}'] = analysis_results
 
-            # Save the analysis results
-            with open(result_path, 'wb') as analysis_pickle:
-                pickle.dump(past_results, analysis_pickle)
+                # Save the analysis results (not sure if we should do this inside of the loop for
+                # cases when the analysis fails because of the limit on http requests)
+                with open(result_path, 'wb') as analysis_pickle:
+                    pickle.dump(past_results, analysis_pickle)
