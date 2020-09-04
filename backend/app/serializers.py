ad23f69fb72a6f8a9565ef500d1781cb38d89c8f
Jason Lin <jasonlin43212@gmail.com>
2020-09-04 16:16:00 -0400

    fix python linter errors

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -3,8 +3,8 @@ Serializers take models or other data structures and present them
 in ways that can be transported across the backend/frontend divide, or
 allow the frontend to suggest changes to the backend/database.
 """
-from rest_framework import serializers
 import json
+from rest_framework import serializers
 from .models import Photo, MapSquare, Photographer, CorpusAnalysisResult
 
 
