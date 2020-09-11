9f035e357de6d9cef8fe91ce626039a300bcfab2
Ryaan Ahmed <rahmed@mit.edu>
2020-09-11 11:02:19 -0400

    add real test for photographer_caption_length

--- backend/app/analysis/tests.py
+++ backend/app/analysis/tests.py
@@ -4,7 +4,13 @@ Tests for the main main.
 
 from django.test import TestCase
 
+# NOTE(ra): we have to use absolute imports in this module because the Django test runner
+# will resolve imports relative to the backend working directory
+# If you do, e.g.,
+#   from ..models import Photo
+# ... you'll crash the test runner. Please don't!
 from app.models import Photo, MapSquare
+from app.analysis import photographer_caption_length
 
 
 class AnalysisTestBase(TestCase):
@@ -12,29 +18,19 @@ class AnalysisTestBase(TestCase):
     Backend TestCase
     """
     def setUp(self):
+        """
+        Setup for all tests -- we initialize a bunch of objects we can use in our tests
+        """
         super().setUp()
-        map_square = MapSquare()
-        map_square.save()
-        photo_0 = Photo(number=1, map_square=map_square)
-        photo_0.save()
-        photo_1 = Photo(number=2, map_square=map_square)
-        photo_1.save()
+        self.map_square = MapSquare()
+        self.map_square.save()
+        self.photo_0 = Photo(number=1, map_square=self.map_square)
+        self.photo_0.save()
+        self.photo_1 = Photo(number=2, map_square=self.map_square)
+        self.photo_1.save()
 
-    def test_sample(self):
-        """
-        Remove me once we have real tests here.
-        """
-        num_photos = Photo.objects.all().count()
-        self.assertEqual(num_photos, 2)
+    def test_photographer_caption_length(self):
+        self.photo_0.photographer_caption = '123456'
+        result = photographer_caption_length.analyze(self.photo_0)
+        self.assertEqual(6, result)
 
-
-class TestCalcWhitespace(AnalysisTestBase):
-    """
-    Tests calc_whitespace
-    """
-
-    def test_analysis(self):
-        """
-        TODO: write me!
-        """
-        pass
