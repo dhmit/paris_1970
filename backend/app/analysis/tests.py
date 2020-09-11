9edd6dcfe067f13ed6d0f46bd1ee4aef5a432963
Ryaan Ahmed <rahmed@mit.edu>
2020-09-11 08:55:41 -0400

    fix backend tests with new db constraint

--- backend/app/analysis/tests.py
+++ backend/app/analysis/tests.py
@@ -4,7 +4,7 @@ Tests for the main main.
 
 from django.test import TestCase
 
-from app.models import Photo
+from app.models import Photo, MapSquare
 
 
 class AnalysisTestBase(TestCase):
@@ -13,9 +13,10 @@ class AnalysisTestBase(TestCase):
     """
     def setUp(self):
         super().setUp()
-        p_0 = Photo()
+        ms_0 = MapSquare()
+        p_0 = Photo(number=1, map_square=ms_0)
         p_0.save()
-        p_1 = Photo()
+        p_1 = Photo(number=2, map_square=ms_0)
         p_1.save()
 
     def test_sample(self):
