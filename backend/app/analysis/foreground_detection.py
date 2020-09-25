037cd2cce4dfc28c893310d1f108e462761d0153
Dylan Weber <34782089+Dylan-Weber@users.noreply.github.com>
2020-09-25 12:33:17 -0700

    Add foreground_dection analysis script

---
+++ backend/app/analysis/foreground_detection.py
@@ -0,0 +1,17 @@
+"""
+
+foreground_detection.py
+
+analysis to extract the foreground of an image and perform analysis on that foreground
+"""
+
+import numpy as np
+import cv2
+
+from app.models import Photo
+
+MODEL = Photo
+
+
+def analyze(photo: Photo):
+    pass
