cd030d7e7f95d7ba62243e34d81b07599b5c2ad5
eebanerjee <eeshab@mit.edu>
2020-09-26 21:43:57 -0500

    Experimented with trivial implementation with Canny edges and Hough lines

--- backend/app/analysis/find_vanishing_point.py
+++ backend/app/analysis/find_vanishing_point.py
@@ -13,15 +13,29 @@ from app.models import Photo
 
 MODEL = Photo
 
+
 def analyze(photo: Photo):
     """
     Calculate the whitespace % for a given Photo
     """
     image = photo.get_image_data()
-
     # Convert image to grayscale
     # (Changes image array shape from (height, width, 3) to (height, width))
     # (Pixels (image[h][w]) will be a value from 0 to 255)
     grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return None
+    edged = auto_canny(grayscale_image)
 
+    return edged
+
+
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+    lines = cv2.HoughLines(edged, 1, np.pi / 180, 50)
+    print(lines[0])
+    # return the edged image
+    return lines
--- backend/app/analysis/tests.py
+++ backend/app/analysis/tests.py
@@ -6,7 +6,10 @@ from pathlib import Path
 
 from django.conf import settings
 from django.test import TestCase
-
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+import os
 # NOTE(ra): we have to use absolute imports in this module because the Django test runner
 # will resolve imports relative to the backend working directory
 # If you do, e.g.,
@@ -16,6 +19,7 @@ from app.models import Photo, MapSquare
 from app.analysis import (
     photographer_caption_length,
     whitespace_percentage,
+    find_vanishing_point,
 )
 
 
@@ -42,6 +46,15 @@ class AnalysisTestBase(TestCase):
 
         # Following photos for find_vanishing_point
         # Team Tangled TODO: import in photo with vanishing point lines
+        self.photo_2 = Photo(number=3, map_square=self.map_square)
+        test_photo_path_2 = Path(settings.TEST_PHOTOS_DIR, '100px_100px-vanishing_point_X.jpg')
+        self.photo_2.front_local_path = test_photo_path_2
+        self.photo_2.save()
+
+        self.photo_3 = Photo(number=4, map_square=self.map_square)
+        test_photo_path_3 = Path(settings.TEST_PHOTOS_DIR, 'bpimg.jpg')
+        self.photo_3.front_local_path = test_photo_path_3
+        self.photo_3.save()
 
     def test_photographer_caption_length(self):
         self.photo_0.photographer_caption = '123456'
@@ -54,6 +67,36 @@ class AnalysisTestBase(TestCase):
 
     def test_find_vanishing_point(self):
         # TODO: get a photo with lines that we can easily analyze/test against
-        # result = find_vanishing_point.analyze(self.PHOTONAME)
+        lines = find_vanishing_point.analyze(self.photo_2)
+
+        # plt.subplot(121), plt.imshow(self.photo_2, cmap='gray')
+        # plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+        # plt.subplot(122), plt.imshow(lines, cmap='gray')
+        # plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+        # plt.show()
+
+        # path = os.path.abspath('100px_100px-vanishing_point_X.jpg')
+        # image = cv2.imread(path)
+        # print(path)
+
+        blank_image = img_1 = np.zeros([100,100,3],dtype=np.uint8)
+
+        for l in lines:
+            rho, theta = l[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+
+            cv2.line(blank_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+        cv2.imshow('image', blank_image)
+        cv2.waitKey()
+        #cv2.imwrite('houghlines.jpg', image)
+        #plt.subplot(122), plt.imshow(image, cmap='gray')
         # assertEquals(expected,output)
         pass
