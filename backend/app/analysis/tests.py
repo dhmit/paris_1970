0e1fc1e5227550ddd4a12f515e5f3fcae06de7bc
Jason Lin <jasonlin43212@gmail.com>
2020-10-02 09:54:34 -0400

    Added tesseract ocr and tested it.

--- backend/app/analysis/tests.py
+++ backend/app/analysis/tests.py
@@ -16,6 +16,7 @@ from app.models import Photo, MapSquare
 from app.analysis import (
     photographer_caption_length,
     whitespace_percentage,
+    text_ocr
 )
 
 
@@ -40,6 +41,10 @@ class AnalysisTestBase(TestCase):
         self.photo_1 = Photo(number=2, map_square=self.map_square)
         self.photo_1.save()
 
+        self.photo_2 = Photo(number=3, map_square=self.map_square)
+        test_photo_path_2 = Path(settings.TEST_PHOTOS_DIR, '300x300_francais.jpg')
+        self.photo_2.front_local_path = test_photo_path_2
+
     def test_photographer_caption_length(self):
         self.photo_0.photographer_caption = '123456'
         result = photographer_caption_length.analyze(self.photo_0)
@@ -48,3 +53,7 @@ class AnalysisTestBase(TestCase):
     def test_whitespace_percentage(self):
         result = whitespace_percentage.analyze(self.photo_0)
         self.assertEqual(50, result)
+
+    def test_text_ocr(self):
+        result = text_ocr.analyze(self.photo_2)
+        self.assertEqual("Français", result)
