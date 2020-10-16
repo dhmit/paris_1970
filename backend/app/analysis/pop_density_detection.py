e0bcdee157124b5471864195249bfdedc12564cd
adanna-a <70187411+adanna-a@users.noreply.github.com>
2020-10-16 12:37:23 -0700

    changes made

--- backend/app/analysis/pop_density_detection.py
+++ backend/app/analysis/pop_density_detection.py
@@ -19,46 +19,37 @@ def analyze(photo: Photo):
     """
     Analysis function that returns the length of the photographer caption for a Photo object
     """
-    # construct the argument parse and parse the arguments
 
-    #ap = argparse.ArgumentParser()
-    #ap.add_argument("-i", "--images", required=True, help="path to images directory")
-    #args = vars(ap.parse_args())
     # initialize the HOG descriptor/person detector
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-    # loop over the image paths
-    #for imagePath in paths.list_images(args["images"]):
 
     # load the image and resize it to (1) reduce detection time
     # and (2) improve detection accuracy
     image = photo.get_image_data()
     image = imutils.resize(image, width=min(400, image.shape[1]))
     orig = image.copy()
-    print("Not here")
-    # detect people in the image
+
+    # detect people in the image, try adjusting parameters before creating new one
     (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
-    print("Here?")
+
     # draw the original bounding boxes
     for (x, y, w, h) in rects:
         cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
     # apply non-maxima suppression to the bounding boxes using a
     # fairly large overlap threshold to try to maintain overlapping
     # boxes that are still people
     rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
     pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
     # draw the final bounding boxes
     for (xA, yA, xB, yB) in pick:
         cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
-    # show some information on the number of bounding boxes
-    #filename = imagePath[imagePath.rfind("/") + 1:]
-    #print("[INFO] {}: {} original boxes, {} after suppression".format(
-    #    filename, len(rects), len(pick)))
-    # show the output images
-    # cv2.imshow("Before NMS", orig)
-    # cv2.imshow("After NMS", image)
-    cv2.imwrite(f'test_pop_density_detection_{photo.id}.jpg', image)
-    #cv2.waitKey(0)
+
+    cv2.imwrite(f'test_pop_density_detection_{photo.id}.jpg', image) #test cases
 
     num_people = len(pick)
+    print(pick)
     return num_people
+
