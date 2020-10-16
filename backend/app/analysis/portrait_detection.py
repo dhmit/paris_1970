79832455268a174ea98be883555c91e6ca5c2d45
Felix Li <felixli5678@gmail.com>
2020-10-16 16:59:04 -0400

    Fixed linter errors

--- backend/app/analysis/portrait_detection.py
+++ backend/app/analysis/portrait_detection.py
@@ -1,3 +1,10 @@
+"""
+
+portrait_detection.py
+
+analysis to calculate if there exists at least 1 face beyond the size of 200 x 200
+"""
+
 import cv2
 from app.models import Photo
 MODEL = Photo
@@ -11,7 +18,8 @@ def analyze(photo: Photo):
     """
 
     # Load classifier and image. Convert image to grayscale for detection
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades
+                                         + 'haarcascade_frontalface_default.xml')
     img = photo.get_image_data()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -19,7 +27,9 @@ def analyze(photo: Photo):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(200, 200))
 
     # Calculate region and area of the face(s)
-    for (x, y, w, h) in faces:
-        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    for (x_coord, y_coord, width, height) in faces:
+        img = cv2.rectangle(img, (x_coord, y_coord),
+                            (x_coord + width, y_coord + height),
+                            (255, 0, 0), 2)
 
     return len(faces) > 0
