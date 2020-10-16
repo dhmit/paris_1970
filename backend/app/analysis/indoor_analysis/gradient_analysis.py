d0b913e4cb2080750c91303eb43b2955bd4ba578
Radha Patel <radha.rpatel02@gmail.com>
2020-10-16 14:34:46 -0500

    Update gradient_analysis.py
    Add docstring and comments to gradient_analysis

--- backend/app/analysis/indoor_analysis/gradient_analysis.py
+++ backend/app/analysis/indoor_analysis/gradient_analysis.py
@@ -1,5 +1,12 @@
+"""
+gradient_analysis.py - analysis to determine if significant vertical brightness gradient exists
+in image
+
+:return: boolean, True if a gradient was found, False otherwise
+"""
+
 import cv2 as cv
-import numpy as np
+import numpy as nps
 from statistics import mean
 
 from app.models import Photo
@@ -10,23 +17,34 @@ WHITESPACE_THRESHOLD = .6
 
 
 def analyze(photo: Photo):
-
+    """
+    Determine if image has vertical gradient. Find regression equation to represent vertical
+    brightness gradient and return True if correlation coeffcient is greater than 0.85.
+    """
+    # Convert image to grayscale
     image = photo.get_image_data()
     grayscale_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     h = len(grayscale_image)
     w = len(grayscale_image[0])
 
+    # Normalize pixel values in image
     normalized_grayscale_image = grayscale_image / np.max(grayscale_image)
 
+    # Divide image into ten horizontal sections and calculate brightness of each by finding
+    # number of pixels with values greater than WHITESPACE_THRESHOLD
+    # Append number to sectioned_pixels list
     sectioned_pixels = []
     for i in range(10):
         section = normalized_grayscale_image[i * (h//10) : (i+1) * (h//10)]
         sectioned_pixels.append((section > WHITESPACE_THRESHOLD).sum())
 
+    #print("row-by-row", sectioned_pixels)
+
     x_vals = [i * (h//10) for i in range(10)]
     xs = np.array(x_vals, dtype=np.float64)
     ys = np.array(sectioned_pixels, dtype=np.float64)
 
+    # Find line of best fit with brightness value from top to bottom of picture
     def best_fit_line(xs, ys):
         m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
              ((mean(xs) ** 2) - mean(xs ** 2)))
@@ -36,6 +54,7 @@ def analyze(photo: Photo):
     def find_squared_error(ys_orig, ys_line):
         return sum((ys_line - ys_orig) * (ys_line - ys_orig))
 
+    # Find correlation coefficient
     def find_r_squared(ys_orig, ys_line):
         y_mean_line = [mean(ys_orig) for y in ys_orig]
         squared_error_regr = find_squared_error(ys_orig, ys_line)
@@ -46,5 +65,10 @@ def analyze(photo: Photo):
     regression_line = [(m * x) + b for x in xs]
     r_squared = find_r_squared(ys, regression_line)
 
+    #print('slope:', m)
+    #print('r-coefficient:', r_squared)
+
+    # Return if there is a high correlation coefficient. Indicates that substantial and
+    # consistent gradient is present.
     return r_squared > 0.85
 
