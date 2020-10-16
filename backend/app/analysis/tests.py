c7d5113886ce4477d0f33eabb8d64d39ecf69bc9
L-Jewel <ljmito42@gmail.com>
2020-10-16 15:32:30 -0500

    Created helper function; fixed a few formatting errors

--- backend/app/analysis/tests.py
+++ backend/app/analysis/tests.py
@@ -38,17 +38,18 @@ class AnalysisTestBase(TestCase):
 
         self.photo_dict = {}
 
-        for i, image in enumerate(os.scandir(settings.TEST_PHOTOS_DIR)):
-            photo_number = i + 1
-            if image.path.endswith('.jpg') and image.is_file():  # Ensures file is an image
-                # Creates Photo object
-                photo = Photo(number=photo_number, map_square=self.map_square)
-                photo.front_local_path = image.path
-                photo.save()
+    def add_photo(self, photo_name):
+        """
+        Creates a photo object and adds it to self.photo_dict given the name of the photo
+        """
+        if photo_name not in self.photo_dict.keys():
+            # Creates Photo object
+            photo = Photo(number=len(self.photo_dict.keys()), map_square=self.map_square)
+            photo.front_local_path = os.path.join(settings.TEST_PHOTOS_DIR, f'{photo_name}.jpg')
+            photo.save()
 
-                # Splits path string to make Photo name
-                photo_name, _ = os.path.splitext(image.name)
-                self.photo_dict[photo_name] = photo
+            # Splits path string to make Photo name
+            self.photo_dict[photo_name] = photo
 
     def test_photographer_caption_length(self):
         """
@@ -56,11 +57,11 @@ class AnalysisTestBase(TestCase):
         """
         expected_values = {'100x100_500px-white_500px-black': 6}
         for image in expected_values:
-            if image in self.photo_dict.keys():
-                self.photo_dict[image].photographer_caption = '123456'
-                result = photographer_caption_length.analyze(self.photo_dict[image])
-                print(f'Caption Length performed on {image}. Result: {result}')
-                self.assertEqual(expected_values[image], result)
+            self.add_photo(image)
+            self.photo_dict[image].photographer_caption = '123456'
+            result = photographer_caption_length.analyze(self.photo_dict[image])
+            print(f'Caption Length performed on {image}. Result: {result}')
+            self.assertEqual(expected_values[image], result)
 
     def test_whitespace_percentage(self):
         """
@@ -68,10 +69,10 @@ class AnalysisTestBase(TestCase):
         """
         expected_values = {'100x100_500px-white_500px-black': 50}
         for image in expected_values:
-            if image in self.photo_dict.keys():
-                result = whitespace_percentage.analyze(self.photo_dict[image])
-                print(f'Whitespace Percentage performed on {image}. Result: {result}')
-                self.assertEqual(expected_values[image], result)
+            self.add_photo(image)
+            result = whitespace_percentage.analyze(self.photo_dict[image])
+            print(f'Whitespace Percentage performed on {image}. Result: {result}')
+            self.assertEqual(expected_values[image], result)
 
     def test_stdev(self):
         """
@@ -80,8 +81,8 @@ class AnalysisTestBase(TestCase):
         expected_values = {'100x76-BlackRectangle': 0, '100x76-GreyRectangle': 0,
                            '100x76-WhiteRectangle': 0, '100x100-BlackSquare': 0,
                            '100x100-GreySquare': 0, '100x100-WhiteSquare': 0,
-                           '100x76-CheckeredRectangle_1': 126, '100x76-CheckeredRectangle_2':
-                               126, '100x76-HalfRectangle_1': 1, '100x76-HalfRectangle_2': 126,
+                           '100x76-CheckeredRectangle_1': 126, '100x76-CheckeredRectangle_2': 126,
+                           '100x76-HalfRectangle_1': 1, '100x76-HalfRectangle_2': 126,
                            '100x76-HalfRectangle_3': 1, '100x76-HalfRectangle_4': 126,
                            '100x100-CheckeredSquare_1': 125, '100x100-CheckeredSquare_2': 125,
                            '100x100-HalfSquare_1': 0, '100x100-HalfSquare_2': 125,
@@ -89,10 +90,10 @@ class AnalysisTestBase(TestCase):
                            '100x100-HalfSquare_4': 125}
 
         for image in expected_values:
-            if image in self.photo_dict.keys():
-                result = stdev.analyze(self.photo_dict[image])
-                print(f'StDev performed on {image}. Result: {result}')
-                self.assertEqual(expected_values[image], int(result))
+            self.add_photo(image)
+            result = stdev.analyze(self.photo_dict[image])
+            print(f'StDev performed on {image}. Result: {result}')
+            self.assertEqual(expected_values[image], int(result))
 
     def test_local_variance(self):
         """
@@ -101,8 +102,9 @@ class AnalysisTestBase(TestCase):
         expected_values = {'100x76-BlackRectangle': 0, '100x76-GreyRectangle': 0,
                            '100x76-WhiteRectangle': 0, '100x100-BlackSquare': 0,
                            '100x100-GreySquare': 0, '100x100-WhiteSquare': 0,
-                           '100x76-CheckeredRectangle_1': 3087, '100x76-CheckeredRectangle_2':
-                               3087, '100x76-HalfRectangle_1': 1717, '100x76-HalfRectangle_2': 1305,
+                           '100x76-CheckeredRectangle_1': 3087,
+                           '100x76-CheckeredRectangle_2': 3087,
+                           '100x76-HalfRectangle_1': 1717, '100x76-HalfRectangle_2': 1305,
                            '100x76-HalfRectangle_3': 1717, '100x76-HalfRectangle_4': 1305,
                            '100x100-CheckeredSquare_1': 2660, '100x100-CheckeredSquare_2': 2660,
                            '100x100-HalfSquare_1': 1305, '100x100-HalfSquare_2': 1305,
@@ -110,10 +112,10 @@ class AnalysisTestBase(TestCase):
                            '100x100-HalfSquare_4': 1305}
 
         for image in expected_values:
-            if image in self.photo_dict.keys():
-                result = local_variance.analyze(self.photo_dict[image])
-                print(f'Local Variance performed on {image}. Result: {result}')
-                self.assertEqual(expected_values[image], int(result))
+            self.add_photo(image)
+            result = local_variance.analyze(self.photo_dict[image])
+            print(f'Local Variance performed on {image}. Result: {result}')
+            self.assertEqual(expected_values[image], int(result))
 
     def test_detail_fft2(self):
         """
@@ -131,10 +133,10 @@ class AnalysisTestBase(TestCase):
                            '100x100-HalfSquare_4': 0}
 
         for image in expected_values:
-            if image in self.photo_dict.keys():
-                result = detail_fft2.analyze(self.photo_dict[image])
-                print(f'FFT performed on {image}. Result: {result}')
-                self.assertEqual(expected_values[image], int(result))
+            self.add_photo(image)
+            result = detail_fft2.analyze(self.photo_dict[image])
+            print(f'FFT performed on {image}. Result: {result}')
+            self.assertEqual(expected_values[image], int(result))
 
     def test_average_detail(self):
         """
@@ -146,7 +148,7 @@ class AnalysisTestBase(TestCase):
                            '100x76-CheckeredRectangle_1': 1030, '100x76-CheckeredRectangle_2': 1030}
 
         for image in expected_values:
-            if image in self.photo_dict.keys():
-                result = average_detail.analyze(self.photo_dict[image])
-                print(f'Average Detail performed on {image}. Result: {result}')
-                self.assertEqual(expected_values[image], int(result))
+            self.add_photo(image)
+            result = average_detail.analyze(self.photo_dict[image])
+            print(f'Average Detail performed on {image}. Result: {result}')
+            self.assertEqual(expected_values[image], int(result))
