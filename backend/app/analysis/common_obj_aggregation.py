82f86f1379c868d849b65fa037b4058eefc0ea1a
Ninger Lei <ninger.lei@gmail.com>
2020-10-23 16:21:42 -0400

    added links

--- backend/app/analysis/common_obj_aggregation.py
+++ backend/app/analysis/common_obj_aggregation.py
@@ -5,6 +5,10 @@ analysis that aggregates other obj detection methods
 takes in the name of the obj we're looking for (for example, words, heads, pedestrians, stop signs)
 returns the number of that specific obj for each photo in the database
 
+links:
+- https://pytorch.org/tutorials/beginner/deep_learning_60min_blitz.html
+- https://www.pyimagesearch.com/2018/11/12/yolo-object-detection-with-opencv/
+
 """
 
 from ..models import Photo
