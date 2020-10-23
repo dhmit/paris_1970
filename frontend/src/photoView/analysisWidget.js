c5cc14f6e168579e01cdfdde369d1970d1b56c18
taokinbo <70776613+taokinbo@users.noreply.github.com>
2020-10-23 16:54:01 -0400

    Created AnalysisWidget Class

---
+++ frontend/src/photoView/analysisWidget.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+
+export class AnalysisWidget extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            view: null
+        }
+    }
+
+}
