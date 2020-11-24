6a1934afcc4f2b6ca796f1e0882d15c6ae444282
sharabhuiyan <sbhuiyan@mit.edu>
2020-11-24 16:24:34 -0500

    removed other object detection modules and venv2 folder

--- backend/app/analysis/faster_rcnn_analysis.py
+++
@@ -1,9 +0,0 @@
-'''
-faster rcnn from: https://debuggercafe.com/faster-rcnn-object-detection-with-pytorch/
-'''
-import torch
-
-import torchvision
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-model.eval()
-
