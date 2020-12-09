6cc0d0726bea34f6796701c29599820d5edfe6ba
Joshua Feliciano <joshfeli613@gmail.com>
2020-12-09 11:43:13 -0500

    Even more linting
    -Added docstring for populate_map_pages.py
    -Fixed typos in populate_map_pages.py and docstring of stdev.py

--- backend/data/populate_map_pages.py
+++ backend/data/populate_map_pages.py
@@ -1,10 +1,21 @@
+"""
+populate_map_pages.py
+
+Pulls from an input csv that details each map page's top right coordinates and
+the map squares it contains
+
+Creates and overwrites an output csv that details the top right coordinates of each map square
+"""
+
 import csv
-# Input CSV - map_Square_page; lowest_map_square; highest_map_square; top_right_lat; top_right_long; is_irregular
+
+# Input CSV - map_Square_page; lowest_map_square; highest_map_square; top_right_lat;
+# top_right_long; is_irregular
 # Output CSV - map_square; top_right_lat; top_right_long
 
 # Map Pages: (5 x 4)
 # change in longitude: 0.01361302273 (width of map page)
-# change in latitutde: 0.011179 (height of map page)
+# change in latitude: 0.011179 (height of map page)
 mp_delta_long = 0.01361302273
 mp_delta_lat = 0.011179
 
@@ -16,10 +27,12 @@ ms_delta_lat = 0.0022358
 
 with open('Map_Page_Input.csv') as input:
     reader = csv.reader(input, delimiter=' ')
+    # Opens in 'write' mode so that the output csv overwrites
+    # each time this runs
     output = open('Map_Page_Output.csv', 'w')
     with output:
         writer = csv.writer(output, delimiter=' ')
-        writer.writerow(["map_square,top_right_lat,top_right_long"]) # Adds title columns
+        writer.writerow(["map_square,top_right_lat,top_right_long"])  # Adds title columns
         for row in reader:
             row = row[0].split(',')  # Splits the row into an array of entries
             if row[-1] != 'FALSE':  # Skips the irregularly shaped map square pages + header
@@ -29,19 +42,23 @@ with open('Map_Page_Input.csv') as input:
             top_right_lat = float(row[3])
             top_right_long = float(row[4])
 
-            current_ms = int(row[1]) # Defines the current map square
+            current_ms = int(row[1])  # Defines the current map square
             # Splits the map page into a 5 x 4 grid and assigns coordinates of map square by
             # referring to the changes in latitude and longitude within the grid
 
-
-            # map pages are 5x4 row x col.
-            # since we start at top right corner coordinate,
-            # subtract incrementally larger values of y*ms_delta_lat (up to down)
+            # Map pages are 5 x 4 grids
+            # Since we start at top right corner coordinate,
+            # subtract incrementally larger values of y*ms_delta_lat (up to down), and
             # subtract incrementally smaller values of x*ms_dela_long (left to right)
             for y in range(0, 5):
                 current_ms_lat = top_right_lat - (y * ms_delta_lat)
-                for x in range(3,-1, -1):
+                for x in range(3, -1, -1):
                     current_ms_long = top_right_long - (x * ms_delta_long)
-                    current_ms_row = '{map_square},{tr_lat},{tr_lng}'.format(map_square = current_ms, tr_lat = current_ms_lat, tr_lng = current_ms_long)
+                    current_ms_row = '{map_square},{tr_lat},{tr_lng}' \
+                        .format(
+                        map_square=current_ms,
+                        tr_lat=current_ms_lat,
+                        tr_lng=current_ms_long
+                    )
                     current_ms += 1
                     writer.writerow([current_ms_row])
