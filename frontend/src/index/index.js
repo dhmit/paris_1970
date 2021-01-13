fe3dbeb6699873d37e6871e8bc2ce16b146f7c16
Jason Lin <jasonlin43212@gmail.com>
2021-01-13 14:44:23 -0500

    Attempted to fix thick borders on map square

--- frontend/src/index/index.js
+++ frontend/src/index/index.js
@@ -80,7 +80,7 @@ class Map extends React.Component {
                                 <Rectangle
                                     key={index}
                                     bounds={mapSquareBounds}
-                                    color={'#b51a00'} weight={4}
+                                    color={'#b51a00'} weight={2}
                                 >
                                     <Popup>
                                         Map Square {index} <br/>
