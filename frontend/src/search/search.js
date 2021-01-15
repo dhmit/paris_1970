6f5e531c257dac12e95147136b3b66d40cde7642
L-Jewel <ljmito42@gmail.com>
2021-01-14 19:39:15 -0600

    Created an actual search bar
    As of now, you can type things in it and click the submit button (which doesn't exactly do too much right now)

--- frontend/src/search/search.js
+++ frontend/src/search/search.js
@@ -2,6 +2,35 @@ import React from 'react';
 import { Navbar, Footer } from '../UILibrary/components';
 // import PropTypes from 'prop-types';
 
+class SearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Search" />
+            </form>
+        );
+    }
+}
+
 export class Search extends React.Component {
     constructor(props) {
         super(props);
@@ -35,6 +64,7 @@ export class Search extends React.Component {
                 <Navbar />
                 <div className='page'>
                     <h1>Search!</h1>
+                    <SearchForm />
                 </div>
                 <Footer />
             </>
