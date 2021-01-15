bbcd37b877be96b3fd45434802346c398886e888
Peihua Huang <nemesis2126@gmail.com>
2021-01-15 14:13:40 -0500

    other links show up

--- frontend/src/UILibrary/components.js
+++ frontend/src/UILibrary/components.js
@@ -154,29 +154,31 @@ export class Navbar extends Component {
 
     render() {
         return (
-          <div>
-              <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                  <a className="navbar-brand"
-                      href="/"><b>This Was Paris in 1970</b></a>
-                  <button className="navbar-toggler" type="button" data-toggle="collapse"
-                      data-target="#navbarNav" aria-controls="navbarNav"
-                      aria-expanded="false" aria-label="Toggle navigation">
-                      <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <ul className="navbar-nav ml-auto">
-                      <li className="collapse bar-collapse" id="navbarNavAltMarkup">
-                          <div className="navbar-nav">
-                              {
-                                  navbarLinks.map((page) => (
-                                      <a key={page.name} className="nav-item nav-link"
-                                          href={page.link}>{page.name}</a>
-                                  ))
-                              }
-                          </div>
-                      </li>
-                  </ul>
-              </nav>
-          </div>
+                      <div>
+                          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                              <a className="navbar-brand" style={{ 'font-size': '200%' }}
+                                  href="/"><b>This Was Paris in 1970</b></a>
+                              <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                  data-target="#navbarNav" aria-controls="navbarNav"
+                                  aria-expanded="false" aria-label="Toggle navigation">
+                                  <span className="navbar-toggler-icon"></span>
+                              </button>
+                              <ul className="navbar-nav ml-auto">
+                                  {
+                                      navbarLinks.map((page) => (
+                                          <li key={page.name} className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                              <div className="navbar-nav">
+                                                  <a className="nav-item nav-link"
+                                                      href={page.link}>{page.name}</a>
+                                              </div>
+                                          </li>
+
+
+                                      ))
+                                  }
+                              </ul>
+                          </nav>
+                      </div>
         );
     }
 }
