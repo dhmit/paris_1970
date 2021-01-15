2768ba9cf7c69b32adba49766f74fb4d93a02bb4
L-Jewel <ljmito42@gmail.com>
2021-01-14 20:50:49 -0600

    Created the skeleton for the form
    There's multiple search options, modeling this site that Ryaan told us about (http://photogrammar.yale.edu/search/)
    
    There's the full text option and the advanced search option (which will let you search by photographer, caption, and then by tags)
    
    Tasks:
    - Filter by analyses (ask Ryaan for clarity)
    - Actually send the query
    - Dropdowns(?)

--- frontend/src/search/search.js
+++ frontend/src/search/search.js
@@ -5,26 +5,59 @@ import { Navbar, Footer } from '../UILibrary/components';
 class SearchForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            keyword: '',
+            photographer: '',
+            caption: '',
+            tags: '',
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({
+            ...this.state,
+            [event.target.name]: event.target.value
+        });
     }
 
     handleSubmit(event) {
         event.preventDefault();
     }
 
+    // When it comes to separating the advanced search and full text search ("normal" search),
+    // should we split the two forms? I think this would work with the same submit button
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
+                <h3>Full Text Search</h3>
                 <label>
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="text" name="keyword" value={this.state.keyword} onChange={this.handleChange} />
                 </label>
+                <input type="submit" value="Search" />
+                <br/><br/>
+
+                <h3>Advanced Search</h3>
+                <label>
+                    <p>Photographer:&nbsp;
+                        <input type="text" name="photographer" value={this.state.photographer} onChange={this.handleChange} />
+                    </p>
+                </label>
+                <br/>
+                <label>
+                    <p>Caption:&nbsp;
+                        <input type="text" name="caption" value={this.state.caption} onChange={this.handleChange} />
+                    </p>
+                </label>
+                <br/>
+                <label>
+                    <p>Tags:&nbsp;
+                        <input type="text" name="tags" value={this.state.tags} onChange={this.handleChange} />
+                    </p>
+                </label>
+                <br/>
                 <input type="submit" value="Search" />
             </form>
         );
@@ -58,12 +91,13 @@ export class Search extends React.Component {
         // }
     }
 
+    // This follows the full text + advanced search model here: http://photogrammar.yale.edu/search/
     render() {
         return (
             <>
                 <Navbar />
                 <div className='page'>
-                    <h1>Search!</h1>
+                    <h1>Search</h1>
                     <SearchForm />
                 </div>
                 <Footer />
