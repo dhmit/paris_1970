e1c9d7eece777186e4dd2b95a1ebe4e866d51f9e
L-Jewel <ljmito42@gmail.com>
2021-01-16 19:54:00 -0600

    Revert "The tags dropdown only appears when you start typing"
    This reverts commit 2c0afb3c3a360e1730bff3a21fe1f9184863bf58.

--- frontend/src/search/search.js
+++ frontend/src/search/search.js
@@ -142,27 +142,23 @@ class SearchForm extends React.Component {
                             onChange={this.handleChange}
                         />
                         <br/>
-
-                        {
-                            this.state.tagFilter.length > 0 ?
-                                <select
-                                    name="tags"
-                                    className='tag-selection'
-                                    multiple={true}
-                                    value={this.state.tags}
-                                    onChange={this.handleMultiSelectChange}
-                                >
-                                    {
-                                        filteredTagData.map((tagData, key) => {
-                                            return (
-                                                <option value={tagData} key={key}>
-                                                    {tagData}
-                                                </option>
-                                            );
-                                        })
-                                    }
-                                </select> : null
-                        }
+                        <select
+                            name="tags"
+                            className='tag-selection'
+                            multiple={true}
+                            value={this.state.tags}
+                            onChange={this.handleMultiSelectChange}
+                        >
+                            {
+                                filteredTagData.map((tagData, key) => {
+                                    return (
+                                        <option value={tagData} key={key}>
+                                            {tagData}
+                                        </option>
+                                    );
+                                })
+                            }
+                        </select>
                     </label>
                     <br/>
                     <input type="submit" value="Search" />
