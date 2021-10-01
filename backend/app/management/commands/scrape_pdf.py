76bfcbc81593b561c1301e894d54101faf2dbd9f
Justice Vidal <jmvidal@mit.edu>
2021-10-01 02:29:32 -0400

    Refactor pdf_scraper
    - scraped first participant list file

--- backend/app/management/commands/scrape_pdf.py
+++ backend/app/management/commands/scrape_pdf.py
@@ -12,7 +12,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 
-def camelot_table(pdf_path, page_range):
+def camelot_table(pdf_path, page_range, flavor="lattice"):
     if page_range == (-1, -1):
         prange = 'all'
     else:
@@ -20,18 +20,18 @@ def camelot_table(pdf_path, page_range):
         prange = f"{start}-{end}"
     camelot_df = (camelot
                   .read_pdf(pdf_path,
-                            flavor="lattice",
+                            flavor=flavor,
                             suppress_stdout=True,
                             pages=prange)
                   )
     return camelot_df
 
 
-def save_pdf_tables(pdf_path, page_range):
-    camelot_df = camelot_table(pdf_path, page_range)
+def save_pdf_tables(pdf_path, page_range, mode="lattice"):
+    camelot_df = camelot_table(pdf_path, page_range, mode)
     pdf_folder, ext = os.path.splitext(pdf_path)
     _, pdf_name = os.path.split(pdf_folder)
-    output_folder = os.path.join(settings.ANALYSIS_DIR, f'arrondissements_tables/{pdf_name}')
+    output_folder = os.path.join(settings.ANALYSIS_DIR, f'extracted_tables/{pdf_name}')
     os.makedirs(
         output_folder,
         exist_ok=True
@@ -56,15 +56,21 @@ class Command(BaseCommand):
             metavar=('start', 'end'),
             default=(-1, -1),
         )
+        parser.add_argument(
+            '--mode',
+            action='store',
+            type=str,
+            default="lattice",
+        )
 
     def handle(self, *args, **options):
         pdf_name = options.get('pdf_name')
         page_range = options.get('page_range')
         pd.set_option('display.max_colwidth', None)
-        arrondissements_folder = os.path.join(settings.ANALYSIS_DIR, 'arrondissements')
+        arrondissements_folder = os.path.join(settings.ANALYSIS_DIR, 'source_pdfs')
         if pdf_name == 'all':
             for pdf in os.scandir(arrondissements_folder):
-                save_pdf_tables(pdf.path, page_range)
+                save_pdf_tables(pdf.path, page_range, options.get('mode'))
         else:
             pdf_path = os.path.join(arrondissements_folder, pdf_name)
-            save_pdf_tables(pdf_path, page_range)
+            save_pdf_tables(pdf_path, page_range, options.get('mode'))
