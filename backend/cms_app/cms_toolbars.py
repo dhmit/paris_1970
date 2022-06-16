5ed6265c0dc8fa4c65548e9a9388438a3a1470ef
JusticeV452 <jmvidal@mit.edu>
2022-06-16 16:40:11 -0400

    Fix 'Home' link

--- backend/cms_app/cms_toolbars.py
+++ backend/cms_app/cms_toolbars.py
@@ -10,7 +10,7 @@ class MyToolbarClass(CMSToolbar):
     def populate(self):
         self.toolbar.add_link_item(  # or add_button(), add_modal_item(), etc
             name='Home',
-            url='blog/'
+            url=''
         )
 
         admin_menu = self.toolbar.get_menu(ADMIN_MENU_IDENTIFIER)
