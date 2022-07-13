778d6ecb52515c9bb87cf0a271bb9a6d67204c29
JusticeV452 <jmvidal@mit.edu>
2022-07-13 16:56:52 -0400

    Update migrations and hide unpublished posts

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -3,7 +3,7 @@ These view functions and classes implement API endpoints
 """
 from django.shortcuts import render
 from blog.models import BlogPost
-from taggit.models import Tag
+from django.http import Http404
 
 
 def index(request):
@@ -11,8 +11,8 @@ def index(request):
     Blog home page
     """
     posts = BlogPost.objects.all()
-    tags = Tag.objects.all()
-    print(posts)
+    # Set of all tags belonging to published posts
+    tags = set([tag for post in posts for tag in post.tags.names() if post.published])
     context = {'posts': posts, 'tags': tags}
     return render(request, 'blog/home.html', context)
 
@@ -22,6 +22,8 @@ def blog_post(request, slug):
     Single blog post view
     """
     post = BlogPost.objects.get(slug=slug)
-    tags = Tag.objects.all()
+    tags = post.tags.names()
     context = {'post': post, 'tags': tags}
+    if not post.published and request.user != post.author:
+        raise Http404
     return render(request, 'blog/post.html', context)
