e2967533a2c395d8dcb803c6492f8ebcf28c9876
Nisha-Nathan <nishabk@mit.edu>
2022-07-18 11:18:44 -0400

    Update creategroups.py

--- backend/blog/management/commands/creategroups.py
+++ backend/blog/management/commands/creategroups.py
@@ -1,5 +1,5 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import Group, Permission, User
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from blog.models import BlogPost
 
