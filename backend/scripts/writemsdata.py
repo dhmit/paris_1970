5b829d249d5fc85ee5f09bd389a17f8e43271e88
Ayden Johnson <adj314@mit.edu>
2022-08-02 15:29:06 -0400

    Model import error fix
    This block of code was added so the MapSquare model could properly be imported.

--- backend/scripts/writemsdata.py
+++ backend/scripts/writemsdata.py
@@ -1,3 +1,11 @@
+import os
+import sys
+newpath = os.getcwd().replace("scripts", "app")
+newpath2 = newpath[:-4]
+#sys.path.insert(0, newpath)
+sys.path.insert(0, newpath2)
+print(sys.path)
+
 from django.db import models
 from app.models import MapSquare
 
