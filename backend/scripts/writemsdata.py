f1385460196d17acc2d93e79694d822d088d72b4
Ayden Johnson <adj314@mit.edu>
2022-08-05 12:04:22 -0400

    More updates for script file
    Just testing different code lines so that the script works.

--- backend/scripts/writemsdata.py
+++ backend/scripts/writemsdata.py
@@ -1,20 +1,28 @@
 import os
 import sys
+
 newpath = os.getcwd().replace("scripts", "app")
 newpath2 = newpath[:-4]
 #sys.path.insert(0, newpath)
 sys.path.insert(0, newpath2)
-print(sys.path)
+#print(sys.path)
 
-from django.db import models
 from app.models import MapSquare
+from app.serializers import MapSquareSerializerWithoutPhotos
+from rest_framework.response import Response
 
 import json
 
 def write_map_square_data():
-    map_square_data = MapSquare.objects.all()
+    map_square_obj = MapSquare.objects.all().values()
+    print(map_square_obj)
+    map_square_data = list(map_square_obj)
+    #ms_data_serialized = MapSquareSerializerWithoutPhotos(map_square_obj, many=True)
+    #map_square_data = Response(ms_data_serialized.data)
 
-    with open("mapsquaredata.json", "w") as file:
-        json.dump(map_square_data, file)
+    with open(r"scripts/mapsquaredata.json", "w") as file:
+        json.dump(map_square_obj, file)
+
 
 write_map_square_data()
+print("write_map_square_data has finished running.")
