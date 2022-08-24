ddc88655070a6fa190f928456f4688c537bd69cf
anastasia <anastasia.aizman@gmail.com>
2022-08-23 21:18:29 -0400

    add - simple photographers view

--- frontend/pages/views/PhotographerView.js
+++ frontend/pages/views/PhotographerView.js
@@ -1,7 +1,6 @@
 import React from "react";
 import * as PropTypes from "prop-types";
 
-import Footer from "../../components/Footer";
 import PhotoViewer from "../../components/PhotoViewer";
 import LoadingPage from "../LoadingPage";
 import ParisMap, {MAPSQUARE_HEIGHT, MAPSQUARE_WIDTH} from "../../components/ParisMap";
@@ -73,10 +72,14 @@ export class PhotographerView extends PhotoViewer {
             <div className="page row">
                 <div className="image-view col-12 col-lg-6">
                     <div className="col-10">
-                        <h5 style={{paddingTop: "1em", fontSize:"28px"}}><strong>Photographer Profile</strong></h5>
+                        <h5 style={{
+                            paddingTop: "1em",
+                            fontSize: "28px"
+                        }}><strong>Photographer Profile</strong></h5>
                         <h1 className="photographer-name">{name}</h1>
                         <div><strong>Number:</strong>{" " + number}</div>
-                        <div><strong>Recorded Sex:</strong>{" " + (recorded_sex ? recorded_sex : "No" +
+                        <div><strong>Recorded
+                            Sex:</strong>{" " + (recorded_sex ? recorded_sex : "No" +
                             " record")}</div>
                         <div><strong>Address:</strong>{" " + (approx_loc ? approx_loc : "No" +
                             " record")}</div>
@@ -119,7 +122,6 @@ export class PhotographerView extends PhotoViewer {
                     )}
                 </div>
             </div>
-            <Footer/>
         </>);
     }
 }
