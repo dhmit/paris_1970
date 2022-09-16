7abcac5b3ebb8e615915469095db2384d642acad
dishakl <24383+dishakl@users.noreply.github.mit.edu>
2022-09-16 16:27:59 -0400

    Added Disha Kohli to About.js

--- frontend/pages/About.js
+++ frontend/pages/About.js
@@ -33,132 +33,132 @@ import Yang_Funing from "../images/team/Yang_Funing.jpg";
 import Zen_Hilary from "../images/team/Zen_Hilary.jpg";
 import Zheng_Vicky from "../images/team/Zheng_Vicky.jpg";
 
-
 const staffMembers = [
     {
         name: "Stephanie Frampton",
         title: "Faculty Director & Associate Professor of Literature",
-        photoSrc: Frampton_Stephanie
+        photoSrc: Frampton_Stephanie,
     },
     {
         name: "Ryaan Ahmed",
         title: "Technical Director & Senior Research Engineer",
-        photoSrc: Ahmed_Ryaan
+        photoSrc: Ahmed_Ryaan,
     },
     {
         name: "Erica Zimmer",
         title: "Research Associate",
-        photoSrc: Zimmer_Erica
+        photoSrc: Zimmer_Erica,
     },
     {
         name: "Cole Fountain",
         title: "Administrative Assistant II",
-        photoSrc: Fountain_Cole
-    }
+        photoSrc: Fountain_Cole,
+    },
 ];
 
 const studentMembers = [
     {
         name: "Adanna Abraham-Igwe",
-        photoSrc: Abraham_Igwe_Adanna
+        photoSrc: Abraham_Igwe_Adanna,
     },
     {
         name: "Tolulope Akinbo",
-        photoSrc: Akinbo_Tolulope
+        photoSrc: Akinbo_Tolulope,
     },
     {
         name: "Eesha Banerjee",
-        photoSrc: Banerjee_Eesha
+        photoSrc: Banerjee_Eesha,
     },
     {
         name: "Joshua Feliciano",
-        photoSrc: Feliciano_Joshua
+        photoSrc: Feliciano_Joshua,
     },
     {
         name: "Raquel Garcia",
-        photoSrc: Garcia_Raquel
+        photoSrc: Garcia_Raquel,
     },
     {
         name: "Montse Garza",
-        photoSrc: Garza_Montse
+        photoSrc: Garza_Montse,
     },
     {
         name: "Michelle He",
-        photoSrc: He_Michelle
+        photoSrc: He_Michelle,
     },
     {
         name: "Peihua Huang",
-        photoSrc: Huang_Peihua
+        photoSrc: Huang_Peihua,
     },
     {
         name: "Nicole Khaimov",
-        photoSrc: Khaimov_Nicole
+        photoSrc: Khaimov_Nicole,
     },
     {
         name: "Ning-Er Lei",
-        photoSrc: Lei_Ning_Er
+        photoSrc: Lei_Ning_Er,
     },
     {
         name: "Felix Li",
-        photoSrc: Li_Felix
+        photoSrc: Li_Felix,
     },
     {
         name: "Jackie Lin",
-        photoSrc: Lin_Jackie
+        photoSrc: Lin_Jackie,
     },
     {
         name: "Jason Lin",
-        photoSrc: Lin_Jason
+        photoSrc: Lin_Jason,
     },
     {
         name: "Radha Patel",
-        photoSrc: Patel_Radha
+        photoSrc: Patel_Radha,
     },
     {
         name: "Lydia Patterson",
-        photoSrc: Patterson_Lydia
+        photoSrc: Patterson_Lydia,
     },
     {
         name: "Sabrina Romero",
-        photoSrc: Romero_Sabrina
+        photoSrc: Romero_Sabrina,
     },
     {
         name: "Justice Vidal",
-        photoSrc: Vidal_Justice
+        photoSrc: Vidal_Justice,
     },
     {
         name: "Rona Wang",
-        photoSrc: Wang_Rona
+        photoSrc: Wang_Rona,
     },
     {
         name: "Yifan Wang",
-        photoSrc: Wang_Yifan
+        photoSrc: Wang_Yifan,
     },
     {
         name: "Dylan Weber",
-        photoSrc: Weber_Dylan
+        photoSrc: Weber_Dylan,
     },
     {
         name: "Funing Yang",
-        photoSrc: Yang_Funing
+        photoSrc: Yang_Funing,
     },
     {
         name: "Hilary Zen",
-        photoSrc: Zen_Hilary
+        photoSrc: Zen_Hilary,
     },
     {
         name: "Vicky Zheng",
-        photoSrc: Zheng_Vicky
-    }
+        photoSrc: Zheng_Vicky,
+    },
+    {
+        name: "Disha Kohli",
+        photoSrc: Ahmed_Ryaan,
+    },
 ];
 
 function TeamMember(props) {
     return (
         <div className="urop-view">
-            <img
-                className="urop-image"
-                src={props.photoSrc}
-            />
+            <img className="urop-image" src={props.photoSrc} />
             <div className="urop-image-overlay">
                 <div className="urop-text">
                     <h4>{props.name}</h4>
@@ -171,9 +171,8 @@ function TeamMember(props) {
 
 TeamMember.propTypes = {
     name: PropTypes.bool,
-    photoSrc: PropTypes.string
+    photoSrc: PropTypes.string,
 };
-
 
 class About extends React.Component {
     constructor(props) {
@@ -181,102 +180,97 @@ class About extends React.Component {
         console.log("getting props?", this.props.text);
     }
     render() {
-        return (<>
-            <div className="page">
-                <TitleDecoratorContainer title={"About"}/>
-                <div className="about-text">
-                    {/*<ul>{this.props.text.map((section, key) => {*/}
-                    {/*    return <li key={key}>*/}
-                    {/*        {section.section === "intro"*/}
-                    {/*            ? <><TitleDecoratorContainer title={section.title}/> hello we're here</>*/}
-                    {/*            : <h2>{section.title}</h2>}*/}
-                    {/*    </li>;*/}
-                    {/*})}</ul>*/}
-                    <div className="prof-img-div text-wrap col-xs-2 col-sm-5 col-md-5 col-lg-3">
-                        <figure className="figure text-center">
-                            <img
-                                className="prof-img"
-                                src={Clark_Catherine}
-                                alt="Catherine Clark"
-                                align="left"
-                            />
-                            <div className="staff-name">Catherine Clark</div>
-                            <figcaption className="figure-caption text-center">
-                                Associate Professor of History and French Studies
-                            </figcaption>
-                        </figure>
+        return (
+            <>
+                <div className="page">
+                    <TitleDecoratorContainer title={"About"} />
+                    <div className="about-text">
+                        {/*<ul>{this.props.text.map((section, key) => {*/}
+                        {/*    return <li key={key}>*/}
+                        {/*        {section.section === "intro"*/}
+                        {/*            ? <><TitleDecoratorContainer title={section.title}/> hello we're here</>*/}
+                        {/*            : <h2>{section.title}</h2>}*/}
+                        {/*    </li>;*/}
+                        {/*})}</ul>*/}
+                        <div className="prof-img-div text-wrap col-xs-2 col-sm-5 col-md-5 col-lg-3">
+                            <figure className="figure text-center">
+                                <img
+                                    className="prof-img"
+                                    src={Clark_Catherine}
+                                    alt="Catherine Clark"
+                                    align="left"
+                                />
+                                <div className="staff-name">Catherine Clark</div>
+                                <figcaption className="figure-caption text-center">
+                                    Associate Professor of History and French Studies
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio eu
+                                feugiat pretium nibh ipsum consequat nisl vel pretium. Malesuada
+                                nunc vel risus commodo viverra maecenas accumsan lacus vel. Cras
+                                pulvinar mattis nunc sed blandit libero volutpat. Aliquam faucibus
+                                purus in massa tempor nec feugiat nisl pretium. Quam nulla porttitor
+                                massa id neque aliquam. Quam quisque id diam vel quam elementum
+                                pulvinar etiam non. Etiam dignissim diam quis enim lobortis
+                                scelerisque fermentum. Fringilla urna porttitor rhoncus dolor purus
+                                non. Vel fringilla est ullamcorper eget nulla facilisi etiam.
+                                Tristique et egestas quis ipsum suspendisse ultrices gravida.
+                                Vehicula ipsum a arcu cursus vitae. Donec et odio pellentesque diam.
+                                Morbi tincidunt ornare massa eget egestas purus viverra accumsan.
+                                Neque vitae tempus quam pellentesque nec nam aliquam sem. Vitae
+                                congue mauris rhoncus aenean.
+                            </p>
+                            <p>
+                                Suspendisse potenti nullam ac tortor. Ac auctor augue mauris augue
+                                neque gravida in. Vitae sapien pellentesque habitant morbi tristique
+                                senectus et netus. Pretium vulputate sapien nec sagittis. Ridiculus
+                                mus mauris vitae ultricies leo integer. Magna sit amet purus gravida
+                                quis blandit turpis cursus. Laoreet non curabitur gravida arcu ac
+                                tortor dignissim convallis aenean. Eget aliquet nibh praesent
+                                tristique magna sit. Tincidunt dui ut ornare lectus. Viverra justo
+                                nec ultrices dui sapien eget mi proin. Nulla facilisi etiam
+                                dignissim diam. Vitae justo eget magna fermentum.
+                            </p>
+                            <p>
+                                Suspendisse potenti nullam ac tortor. Ac auctor augue mauris augue
+                                neque gravida in. Vitae sapien pellentesque habitant morbi tristique
+                                senectus et netus. Pretium vulputate sapien nec sagittis. Ridiculus
+                                mus mauris vitae ultricies leo integer. Magna sit amet purus gravida
+                                quis blandit turpis cursus. Laoreet non curabitur gravida arcu ac
+                                tortor dignissim convallis aenean. Eget aliquet nibh praesent
+                                tristique magna sit. Tincidunt dui ut ornare lectus. Viverra justo
+                                nec ultrices dui sapien eget mi proin. Nulla facilisi etiam
+                                dignissim diam. Vitae justo eget magna fermentum.
+                            </p>
+                        </div>
                     </div>
-                    <div>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio
-                            eu feugiat pretium nibh ipsum consequat nisl vel pretium. Malesuada
-                            nunc vel risus commodo viverra maecenas accumsan lacus vel. Cras
-                            pulvinar mattis nunc sed blandit libero volutpat. Aliquam faucibus
-                            purus in massa tempor nec feugiat nisl pretium. Quam nulla porttitor
-                            massa id neque aliquam. Quam quisque id diam vel quam elementum
-                            pulvinar etiam non. Etiam dignissim diam quis enim lobortis scelerisque
-                            fermentum. Fringilla urna porttitor rhoncus dolor purus non. Vel
-                            fringilla est ullamcorper eget nulla facilisi etiam. Tristique et
-                            egestas quis ipsum suspendisse ultrices gravida. Vehicula ipsum a arcu
-                            cursus vitae. Donec et odio pellentesque diam. Morbi tincidunt ornare
-                            massa eget egestas purus viverra accumsan. Neque vitae tempus quam
-                            pellentesque nec nam aliquam sem. Vitae congue mauris rhoncus aenean.
-                        </p>
-                        <p>
-                            Suspendisse potenti nullam ac tortor. Ac auctor augue mauris augue
-                            neque gravida in. Vitae sapien pellentesque habitant morbi tristique
-                            senectus et netus. Pretium vulputate sapien nec sagittis. Ridiculus
-                            mus mauris vitae ultricies leo integer. Magna sit amet purus gravida
-                            quis blandit turpis cursus. Laoreet non curabitur gravida arcu ac
-                            tortor dignissim convallis aenean. Eget aliquet nibh praesent tristique
-                            magna sit. Tincidunt dui ut ornare lectus. Viverra justo nec ultrices
-                            dui sapien eget mi proin. Nulla facilisi etiam dignissim diam. Vitae
-                            justo eget magna fermentum.
-                        </p>
-                        <p>
-                            Suspendisse potenti nullam ac tortor. Ac auctor augue mauris augue
-                            neque gravida in. Vitae sapien pellentesque habitant morbi tristique
-                            senectus et netus. Pretium vulputate sapien nec sagittis. Ridiculus
-                            mus mauris vitae ultricies leo integer. Magna sit amet purus gravida
-                            quis blandit turpis cursus. Laoreet non curabitur gravida arcu ac
-                            tortor dignissim convallis aenean. Eget aliquet nibh praesent tristique
-                            magna sit. Tincidunt dui ut ornare lectus. Viverra justo nec ultrices
-                            dui sapien eget mi proin. Nulla facilisi etiam dignissim diam. Vitae
-                            justo eget magna fermentum.
-                        </p>
-                    </div>
-                </div>
-                <div className="team">
-                    <div className="about-title">Staff Members</div>
-                    <div className="row justify-content-center">
-                        {staffMembers.map((member, k) => (
-                            <TeamMember
-                                key={k}
-                                name={member.name}
-                                photoSrc={member.photoSrc}
-                            />
-                        ))}
-                    </div>
-                    <div className="about-title">UROP Members</div>
-                    <div className="row justify-content-center">
-                        {studentMembers.map((member, k) => (
-                            <TeamMember
-                                key={k}
-                                name={member.name}
-                                photoSrc={member.photoSrc}
-                            />
-                        ))}
+                    <div className="team">
+                        <div className="about-title">Staff Members</div>
+                        <div className="row justify-content-center">
+                            {staffMembers.map((member, k) => (
+                                <TeamMember key={k} name={member.name} photoSrc={member.photoSrc} />
+                            ))}
+                        </div>
+                        <div className="about-title">UROP Members</div>
+                        <div className="row justify-content-center">
+                            {studentMembers.map((member, k) => (
+                                <TeamMember key={k} name={member.name} photoSrc={member.photoSrc} />
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <Footer/>
-        </>);
+                <Footer />
+            </>
+        );
     }
 }
 
 About.propTypes = {
-    text: PropTypes.array
+    text: PropTypes.array,
 };
 
 export default About;
