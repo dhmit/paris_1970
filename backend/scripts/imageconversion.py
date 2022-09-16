2b4b92aa5f91c670eec4a0a9c2d360d4edcee049
Ryaan Ahmed <mail@ryaanahmed.com>
2022-09-16 10:58:48 -0400

    Update imageconversion to do whole dataset in parallel

--- backend/scripts/imageconversion.py
+++ backend/scripts/imageconversion.py
@@ -1,65 +1,65 @@
 import os
 import subprocess
 import sys
+from pathlib import Path
 
 from tqdm import tqdm
 
 
-def convert(old_path, new_path, old_filetype, new_filetype, commands=None):
+def convert(src_dir_path, dest_dir_path, commands=['-quality', '20%']):
     """
     Python script for image conversion using Image Magick
     
-    :param old_path: (str) path to directory containing images
-            [ex. 'Users/bob/Desktop/']
-    :param new_path: (str) path where output is intended to be saved in
-            [ex. 'Users/bob/Desktop/new/']
-    :param old_filetype: (str) image format of input images
-            [ex. 'TIF']
-    :param new_filetype: (str) desired output image format
-            [ex. 'jpg']
-    :param commands: (list) commands to apply to images, default = None
-            [ex. ['-quality, '20%']
+    :param src_dir_path: Path object to directory containing subdirectories of images
+    :param dest_dir_path: Path path object where output is intended to be saved in
+    :param commands: (list of strings) commands to apply to images
     :return: None, output produced in new directory
     """
 
-    # creating new directory for output
-    try:
-        os.mkdir(new_path)
-    except FileExistsError:
-        pass
+    magick_commands = []
+    for src_sub_dir in src_dir_path.iterdir():
+        if not src_sub_dir.is_dir(): continue
 
-    # str of commands to apply to images
-    command_str = ''
-    for i in commands:
-        command_str += i + ' '
+        dest_sub_dir = Path(dest_dir_path, src_sub_dir.name)
+        if not dest_sub_dir.exists():
+            dest_sub_dir.mkdir()
 
-    # looping through old directory
-    for tif_path in tqdm(os.listdir(old_path), bar_format='{l_bar}{bar:30}{r_bar}{bar:-10b}'):
-        old_type = os.path.join(old_path, tif_path)
-        if os.path.isfile(old_type) and old_type.endswith(old_filetype):
-            # formatting new filename ex. '/square.jpg'
-            name = os.path.splitext(tif_path)[0] + "." + new_filetype
-            new_type = os.path.join(new_path, name)
+        magick_args = ' ' .join(commands)
+
+        # looping through old directory
+        for src_image_path in src_sub_dir.iterdir():
+            # print(src_image_path)
+            if src_image_path.suffix.lower() != '.jp2' or not src_image_path.is_file():
+                print(f"Skipping {src_image_path} because it is not a .jp2 file\n")
+                continue
+
+            dest_image_path = Path(dest_sub_dir, src_image_path.stem + '.jpg')
+            if dest_image_path.exists(): continue
 
             # formatting command
-            # ex. magick Users/bob/Desktop/old/square.jpg -quality 20%
-            # Users/bob/Desktop/new/square.jpg
-            cmd = f'magick {old_type} {command_str}{new_type}'
-            subprocess.run(cmd, shell=True, capture_output=True, check=True)
+            # ex. magick Users/bob/Desktop/old/square.jpg -quality 20% Users/bob/Desktop/new/square.jpg
+            cmd = f'magick "{src_image_path}" {magick_args} "{dest_image_path}"'
+            magick_commands.append(cmd)
 
+    # Once we compute all the commands, we use subprocess.Popen to run them in parallel
+    batch_size = 35
+    for min_job_idx in range(0, len(magick_commands), batch_size):
+        max_job_idx = min_job_idx+batch_size
+        batch_cmds = magick_commands[min_job_idx:max_job_idx]
+        print("--------------------------------------------------------------------------------")
+        print(f"| Processing {min_job_idx} to {max_job_idx} of {len(magick_commands)}")
+        print("--------------------------------------------------------------------------------")
+        procs = [subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) for cmd in batch_cmds]
+        for proc in tqdm(procs): proc.wait()
 
 if __name__ == "__main__":
-    script = sys.argv[0]  # python imageconversion.py
-    old_path_arg = sys.argv[1]  # argument 1 old_path
-    new_path_arg = sys.argv[2]  # argument 2 new_path
-    old_file_arg = sys.argv[3]  # argument 3 old_filetype
-    new_file_arg = sys.argv[4]  # argument 4 new_filetype
-    commands_arg = sys.argv[5:]  # argument 5 commands
-    convert(old_path_arg, new_path_arg, old_file_arg, new_file_arg, commands_arg)
+    src_dir_path = Path(sys.argv[1])
+    dest_dir_path = Path(sys.argv[2])
+    commands_arg = sys.argv[3:]
+
+    convert(src_dir_path, dest_dir_path, commands_arg)
 
 """
 # Example of command-line:
-
     python scripts/imageconversion.py /Users/bob/Desktop/TestImages /Users/bob/Desktop/newtest
-    TIF JPG -thumbnail 300x90
 """
