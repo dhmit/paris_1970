c617b3ecdb5e3c6572c4ae9210053d10f998bb7f
Mena <58854232+Menamonmon@users.noreply.github.com>
2022-10-07 16:12:19 -0400

    added a search bar to the photographers page (currently capped at 50 results per search query) will implement pagination in a future pr

--- frontend/pages/views/PhotographerListView.js
+++ frontend/pages/views/PhotographerListView.js
@@ -1,23 +1,39 @@
 import React from "react";
 import Footer from "../../components/Footer";
 import * as PropTypes from "prop-types";
-
+import { debounce } from "../../common";
 
 export class PhotographerListView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            photographers: JSON.parse(this.props.photographers)
+            photographers: [],
         };
     }
 
     hrefFunc(number) {
         return `/photographer/${number}`;
-    };
+    }
 
     srcFunc(number) {
         return `${this.props.photoListDir}/${number}_photo.jpg`;
-    };
+    }
+
+    updatePhotographers(name) {
+        debounce(async () => {
+            const fetchedPhotographers = await this.fetchPhotographers(name);
+            this.setState({ photographers: fetchedPhotographers });
+        }, 300)();
+    }
+
+    async fetchPhotographers(sq) {
+        try {
+            const res = await fetch(`/api/search_photographers?name=${sq}`);
+            return res.json();
+        } catch {
+            return [];
+        }
+    }
 
     getPhotoList() {
         const photoSize = [100, 100];
@@ -25,44 +41,57 @@ export class PhotographerListView extends React.Component {
             return (
                 <li className="col-2 col-lg-2 one-photographer list-inline-item" key={k}>
                     <div className="child">
-                        <a key={k}
-                           href={this.hrefFunc(photog.number)}>
+                        <a key={k} href={this.hrefFunc(photog.number)}>
                             <img
                                 alt={photog.number}
                                 width={photoSize[0]}
-                                src={this.srcFunc(photog.number)}/>
+                                src={this.srcFunc(photog.number)}
+                            />
                         </a>
                         <p>{photog.name ? photog.name : "No Name"}</p>
                     </div>
                 </li>
             );
         });
+    }
 
+    componentDidMount() {
+        this.updatePhotographers("");
     }
 
     render() {
         return (
             <>
                 <div className="row">
-                    <p style={{
-                        fontSize: "30px",
-                        fontWeight: "600"
-                    }}>Photographers</p>
+                    <p
+                        style={{
+                            fontSize: "30px",
+                            fontWeight: "600",
+                        }}
+                    >
+                        Photographers
+                    </p>
+                </div>
+                <div>
+                    Search By Name:{" "}
+                    <input
+                        onChange={(e) => {
+                            this.updatePhotographers(e.target.value);
+                        }}
+                    />
                 </div>
                 <div className="row">
                     <ul className="list-inline">{this.getPhotoList()}</ul>
                 </div>
                 <div>
-                    <Footer/>
+                    <Footer />
                 </div>
             </>
         );
     }
 }
 
-
 PhotographerListView.propTypes = {
     photoListDir: PropTypes.string,
-    photographers: PropTypes.string
+    photographers: PropTypes.string,
 };
-
