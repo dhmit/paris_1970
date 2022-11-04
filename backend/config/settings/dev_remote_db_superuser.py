7191a0f37d9cfd8261af3d76ec516d880b8af5c3
Ryaan Ahmed <rahmed@mit.edu>
2022-11-04 11:19:13 -0400

    rename 'admin' to 'remote superuser' in settings file

--- backend/config/settings/dev_remote_db_superuser.py
+++ backend/config/settings/dev_remote_db_superuser.py
@@ -4,12 +4,16 @@ Local development Django settings for dhmit/paris_1970
 
 Under no circumstances run the server with these settings in production!
 
+This settings file is for running the remote DB with superuser privileges.
+
+Only DH staff have these credentials. Ask Ryaan or Jeffrey if you think you need to use this.
+
 """
 
 
 from .dev import *  # pylint: disable=unused-wildcard-import, wildcard-import
 
-pw_path = Path(PROJECT_ROOT, 'admin_db_password.txt')
+pw_path = Path(PROJECT_ROOT, 'remote_db_superuser_password.txt')
 with open(pw_path, 'r', encoding='utf-8') as pw_file:
     db_pw = pw_file.readline().strip()
 
