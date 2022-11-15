626f6156d6050b71048d587daf20c37ecb2919c1
Ryaan Ahmed <mail@ryaanahmed.com>
2022-11-15 15:26:18 -0500

    start cleaning up the analysis pipeline

--- backend/app/management/commands/runanalysis.py
+++ backend/app/management/commands/runanalysis.py
@@ -5,7 +5,6 @@ Syncs local db with data from project Google Sheet
 """
 
 import sys
-import pickle
 import os
 import json
 
@@ -16,25 +15,32 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from app.common import print_header
-from app.models import PhotoAnalysisResult
+from app.models import Photo, PhotoAnalysisResult
+
+
+def photo_id_str(photo):
+    return f'{photo.map_square.number}_{photo.folder}-{photo.number}'
 
 
 class Command(BaseCommand):
     """
-    Custom django-admin command used to run an analysis from the app/analysis folder
+    Custom django-admin command used to run an analysis over the whole dataset from the app/analysis folder
+    optionally overwriting existing work
     """
     help = 'Run an analysis'
 
     def add_arguments(self, parser):
         parser.add_argument('analysis_name', action='store', type=str)
-        parser.add_argument('--use_pickled', action='store_true')
         parser.add_argument('--run_one', action='store_true')
+        parser.add_argument('--overwrite', action='store_true')
+        parser.add_argument('--verbose', action='store_true')
 
     def handle(self, *args, **options):
         # pylint: disable=too-many-locals
         analysis_name = options.get('analysis_name')
-        use_pickled = options.get('use_pickled')
         run_one = options.get('run_one')
+        overwrite = options.get('overwrite')
+        verbose = options.get('verbose')
 
         try:
             analysis_module = import_module(f'.{analysis_name}', package='app.analysis')
@@ -43,85 +49,51 @@ class Command(BaseCommand):
             print_header('There is no analysis with that name.')
             sys.exit(1)
 
-        if analysis_module:
-            # Make sure local "ANALYSIS_PICKLE_PATH" exists before attempting to read or write
-            os.makedirs(settings.ANALYSIS_PICKLE_PATH, exist_ok=True)
+        analysis_func: Callable[[object], dict] = getattr(analysis_module, 'analyze')
 
-            result_path = os.path.join(settings.ANALYSIS_PICKLE_PATH, f'{analysis_name}.pickle')
-            if os.path.exists(result_path):
-                with open(result_path, 'rb') as analysis_pickle:
-                    stored_results = pickle.load(analysis_pickle)
-            else:
-                stored_results = {}
+        if run_one:
+            # in a list because this has to be iterable for the loop below...
+            photos = [Photo.objects.first()]
+        else:
+            photos = Photo.objects.all()
 
-            analysis_func: Callable[[object], dict] = getattr(analysis_module, 'analyze')
-            model = getattr(analysis_module, 'MODEL')
+        print_header(f'Running {analysis_name} on {photos.count()} photos')
+        skipped = []
+        errors = []
+        photos_done = 0
+        for photo in photos:
 
-            # TODO(ra): currently we assume all analyses are on Photos
-            # Eventually we want to generalize to include analyses on MapSquares and Photographers
-            analysis_result_model = PhotoAnalysisResult
+            if verbose:
+                print(f'\nRunning on {photo_id_str(photo)}')
+            elif photos_done % 100 == 0:
+                print(f'\nAnalyzed {photos_done} so far...')
 
-            # delete existing db instances
-            analysis_result_model.objects.filter(name=analysis_name).delete()
 
-            if run_one:
-                # in a list because this has to be iterable for the loop below...
-                model_instances = [model.objects.first()]
-            else:
-                model_instances = model.objects.all()
+            if not photo.has_valid_source():
+                print(f'Photo {photo_id_str(photo)} - could not find a source image file. Skipping.')
+                continue
+            elif verbose:
+                print(photo.image_local_filepath())
 
-            num_computed = 0
-            save_threshold = 20  # Number of analyses that need to be done before pickling
-
-            for model_instance in model_instances:
-                if not model_instance.has_valid_source():
-                    continue
-                # NOTE: These identifiers assume that the photo number and map square number are
-                #       not None
-                print_msg = f'Running {analysis_name} on {model} {model_instance.id} ' \
-                            f'(Photo number: {model_instance.number}, ' \
-                            f'Map square: {model_instance.map_square.number})'
-                instance_identifier = f'photo_{model_instance.number}_' \
-                                      f'{model_instance.map_square.number}'
-
-                should_run_analysis = True
-                if use_pickled:
-                    if instance_identifier in stored_results:
-                        print(f'Using stored results on (Photo number: {model_instance.number}, '
-                              f'Map square: {model_instance.map_square.number})')
-                        result = stored_results[instance_identifier]
-                        should_run_analysis = False
-                    else:
-                        print('No stored result was found, so recomputing.')
-
-                if should_run_analysis:
-                    print(print_msg)
-                    try:
-                        result = analysis_func(model_instance)
-                        analysis_result = analysis_result_model(
-                            name=analysis_name,
-                            result=json.dumps(result),
-                            photo=model_instance,
-                        )
-                        analysis_result.save()
-
-                        # Store the result
-                        stored_results[instance_identifier] = result
-                        num_computed += 1
-
-                        # Quick save the analysis results so far in case of failure
-                        if num_computed == save_threshold:
-                            num_computed = 0
-                            with open(result_path, 'wb+') as analysis_pickle:
-                                pickle.dump(stored_results, analysis_pickle)
-                    except Exception as e:  # pylint: disable=bare-except
-                        print('Error:', e)
-                        print(f'Photo number {model_instance.number} failed. Skipping.')
-
-            # Save the analysis stored_results
-            # TODO: handle case where analysis fails (this won't pickle if something fails)
             try:
-                with open(result_path, 'wb+') as analysis_pickle:
-                    pickle.dump(stored_results, analysis_pickle)
-            except:  # pylint: disable=bare-except
-                pass
+                existing_analysis = PhotoAnalysisResult.objects.get(name=analysis_name, photo=photo)
+            except PhotoAnalysisResult.DoesNotExist:
+                existing_analysis = None
+
+            if existing_analysis:
+                if overwrite:
+                    existing_analysis.delete()
+                else:
+                    print(f'Photo {photo_id_str(photo)} - an analysis exists and we are not overwriting. Skipping.')
+                    continue
+
+            try:
+                result = analysis_func(photo)
+                result_json = json.dumps(result)
+                analysis_result = PhotoAnalysisResult(name=analysis_name, result=result_json, photo=photo)
+                analysis_result.save()
+            except Exception as e:  # pylint: disable=bare-except
+                err_msg = f'Photo {photo_id_str(photo)} - an analysis exists and we are not overwriting. Error: {e}. Skipping.'
+                print(err_msg)
+
+            photos_done += 1
