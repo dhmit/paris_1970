3e516c45b44dc34f14200ecf4a7386dfccf3ea23
Ryaan Ahmed <mail@ryaanahmed.com>
2022-11-17 16:14:53 -0500

    lint

--- backend/app/views.py
+++ backend/app/views.py
@@ -6,7 +6,6 @@ import json
 import os
 import re
 
-from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
