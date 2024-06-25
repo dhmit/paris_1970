eae2632227439022b8de028d47a455853874823b
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2024-06-25 16:49:10 -0400

    Moved explore url outside of i18n patterns
    - Fixes redirect that breaks search page

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -36,129 +36,130 @@ def react_view_path(route, component_name):
     )
 
 
-urlpatterns = i18n_patterns(
-    # Django admin page
-    path('admin/', admin.site.urls),
-
-    # Translation
-    path('rosetta/', include('rosetta.urls')),
-    path("locales/<str:language_code>/", api_views.translation, name="translation"),
-
-    ################################################################################
-    # API endpoints
-    ################################################################################
-    # Photos
-    path('api/photo/<int:map_square_number>/<int:folder_number>/<int:photo_number>/',
-         api_views.photo,
-         name="photo"),
-
-    path('api/prev_next_photos/<int:map_square_number>/<int:folder_number>/<int:photo_number>/',
-         api_views.previous_next_photos,
-         name="previous_next_photos"),
-
-    path(
-        'api/similar_photos/<int:map_square_number>/<int:folder_number>/<int:photo_number>/<int:num_similar_photos>/',
-        api_views.get_photo_by_similarity,
-        name="similar_photos"
-    ),
-
-    path('api/all_photos/',
-         api_views.all_photos,
-         name="all_photos"),
-
-    path('api/random_photos/',
-         api_views.get_random_photos,
-         name="random_photos"),
-
+urlpatterns = [
     # explore
     path('api/explore/', api_views.explore, name="explore"),
+    path("locales/<str:language_code>/", api_views.translation, name="translation")
+    
+] + i18n_patterns(
+     # Django admin page
+     path('admin/', admin.site.urls),
 
-    # Photographers
-    path('api/search_photographers/',
-         api_views.search_photographers),
+     # Translation
+     path('rosetta/', include('rosetta.urls')),
 
-    path('api/search_photographers/dropdown_options',
-         api_views.get_search_photographers_dropdown_options),
+     ################################################################################
+     # API endpoints
+     ################################################################################
+     # Photos
+     path('api/photo/<int:map_square_number>/<int:folder_number>/<int:photo_number>/',
+          api_views.photo,
+          name="photo"),
 
-    path('api/photographer/',
-         api_views.get_photographer,
-         name="all_photographers"),
+     path('api/prev_next_photos/<int:map_square_number>/<int:folder_number>/<int:photo_number>/',
+          api_views.previous_next_photos,
+          name="previous_next_photos"),
 
-    path('api/photographer/<int:photographer_number>/', api_views.get_photographer,
-         name='photographer'),
+     path(
+          'api/similar_photos/<int:map_square_number>/<int:folder_number>/<int:photo_number>/<int:num_similar_photos>/',
+          api_views.get_photo_by_similarity,
+          name="similar_photos"
+     ),
 
-    # Map Squares
-    path('api/map_square/<int:map_square_number>/',
-         api_views.get_map_square,
-         name="map_square"),
+     path('api/all_photos/',
+          api_views.all_photos,
+          name="all_photos"),
 
-    path('api/all_map_squares/',
-         api_views.all_map_squares,
-         name="all_map_squares"),
+     path('api/random_photos/',
+          api_views.get_random_photos,
+          name="random_photos"),
 
-    # Tags
-    path('api/tag/<str:tag_name>/',
-         api_views.get_photos_by_tag,
-         name="get_photos_by_tag"),
+     # Photographers
+     path('api/search_photographers/',
+          api_views.search_photographers),
 
-    # Analyses
-    path('api/all_analyses/', api_views.all_analyses, name='all_analyses'),
-    path('api/similarity/', api_views.get_all_photos_in_order, name="all_photos_in_order"),
-    path('api/analysis/<str:analysis_name>/', api_views.get_photos_by_analysis,
-         name="get_photos_by_analysis"),
-    path('api/analysis/<str:analysis_name>/<str:object_name>/', api_views.get_photos_by_analysis,
-         name="get_photos_by_analysis"),
-    path('api/corpus_analysis/', api_views.get_corpus_analysis_results, name="get_corpus"),
-    path('api/clustering/<int:number_of_clusters>/<int:cluster_number>/',
-         api_views.get_photos_by_cluster, name="clustering"),
-    path('api/text_ocr/', api_views.get_images_with_text),
+     path('api/search_photographers/dropdown_options',
+          api_views.get_search_photographers_dropdown_options),
+
+     path('api/photographer/',
+          api_views.get_photographer,
+          name="all_photographers"),
+
+     path('api/photographer/<int:photographer_number>/', api_views.get_photographer,
+          name='photographer'),
+
+     # Map Squares
+     path('api/map_square/<int:map_square_number>/',
+          api_views.get_map_square,
+          name="map_square"),
+
+     path('api/all_map_squares/',
+          api_views.all_map_squares,
+          name="all_map_squares"),
+
+     # Tags
+     path('api/tag/<str:tag_name>/',
+          api_views.get_photos_by_tag,
+          name="get_photos_by_tag"),
+
+     # Analyses
+     path('api/all_analyses/', api_views.all_analyses, name='all_analyses'),
+     path('api/similarity/', api_views.get_all_photos_in_order, name="all_photos_in_order"),
+     path('api/analysis/<str:analysis_name>/', api_views.get_photos_by_analysis,
+          name="get_photos_by_analysis"),
+     path('api/analysis/<str:analysis_name>/<str:object_name>/', api_views.get_photos_by_analysis,
+          name="get_photos_by_analysis"),
+     path('api/corpus_analysis/', api_views.get_corpus_analysis_results, name="get_corpus"),
+     path('api/clustering/<int:number_of_clusters>/<int:cluster_number>/',
+          api_views.get_photos_by_cluster, name="clustering"),
+     path('api/text_ocr/', api_views.get_images_with_text),
 
 
-    # Arrondissements
-    path('api/arrondissements_geojson/', api_views.get_arrondissements_geojson,
-         name="get_arrondissement"),
-    path('api/arrondissements_geojson/<int:arr_number>/',
-         api_views.get_arrondissements_geojson, name="get_one_arrondissement"),
-    path('api/arrondissements_map_squares/', api_views.get_arrondissements_map_squares),
-    path('api/arrondissements_map_squares/<int:arr_number>', api_views.get_arrondissements_map_squares),
+     # Arrondissements
+     path('api/arrondissements_geojson/', api_views.get_arrondissements_geojson,
+          name="get_arrondissement"),
+     path('api/arrondissements_geojson/<int:arr_number>/',
+          api_views.get_arrondissements_geojson, name="get_one_arrondissement"),
+     path('api/arrondissements_map_squares/', api_views.get_arrondissements_map_squares),
+     path('api/arrondissements_map_squares/<int:arr_number>', api_views.get_arrondissements_map_squares),
 
-    # Distances
-    path('api/get_photo_distances/<int:photographer_number>/',
-         api_views.get_photo_distances, name="get_photo_distances"),
+     # Distances
+     path('api/get_photo_distances/<int:photographer_number>/',
+          api_views.get_photo_distances, name="get_photo_distances"),
 
 
-    ################################################################################
-    # View Pages
-    ################################################################################
-    path('', views.index),
-    path('map/', views.map_page),
-    path('about/', views.about),
-    path('search/', views.search_view),
-    path('explore/', views.explore_view),
-    # Photographers
-    path('photographers/', views.photographer_list_view),
-    path('photographer/<int:photographer_number>/', views.photographer_view),
+     ################################################################################
+     # View Pages
+     ################################################################################
+     path('', views.index),
+     path('map/', views.map_page),
+     path('about/', views.about),
+     path('search/', views.search_view),
+     path('explore/', views.explore_view),
+     # Photographers
+     path('photographers/', views.photographer_list_view),
+     path('photographer/<int:photographer_number>/', views.photographer_view),
 
-    # Photos
-    path('photo/<int:map_square_number>/<int:folder_number>/<int:photo_number>/', views.photo_view),
+     # Photos
+     path('photo/<int:map_square_number>/<int:folder_number>/<int:photo_number>/', views.photo_view),
 
-    # blog urls
-    path(f'{settings.BLOG_ROOT_URL}/', blog_views.blog_home_page, name="blog_home"),
-    path(f'{settings.BLOG_ROOT_URL}/<str:slug>/', blog_views.blog_post,
-         name='blog-detail'),
+     # blog urls
+     path(f'{settings.BLOG_ROOT_URL}/', blog_views.blog_home_page, name="blog_home"),
+     path(f'{settings.BLOG_ROOT_URL}/<str:slug>/', blog_views.blog_post,
+          name='blog-detail'),
 
-    # Map Squares
-    path('map_square/<int:map_square_number>/', views.map_square_view),
-    path('text_ocr/', views.text_ocr_view),
-    path('similarity/<int:map_square_number>/<int:folder_number>/<int:photo_number>/', views.similar_photos_view),
+     # Map Squares
+     path('map_square/<int:map_square_number>/', views.map_square_view),
+     path('text_ocr/', views.text_ocr_view),
+     path('similarity/<int:map_square_number>/<int:folder_number>/<int:photo_number>/', views.similar_photos_view),
 
-    # path('clustering/<int:num_of_clusters>/<int:cluster_num>/', views.cluster_view),
-    # Blog urls
-    # Log in/out urls
-    path('login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # Tags
-    path('tag/<str:tag_name>/', views.tag_view),
-    path('tag/<str:tag_name>/<int:page>/', views.tag_view),
+     # path('clustering/<int:num_of_clusters>/<int:cluster_num>/', views.cluster_view),
+     # Blog urls
+     # Log in/out urls
+     path('login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
+     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     # Tags
+     path('tag/<str:tag_name>/', views.tag_view),
+     path('tag/<str:tag_name>/<int:page>/', views.tag_view),
 
 )
