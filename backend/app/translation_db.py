5fe6bff442d1b2661810944248f06bd5c1b141ce
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2024-06-25 15:51:29 -0400

    Added object tag translation functions
    - Also added other translation entries

--- backend/app/translation_db.py
+++ backend/app/translation_db.py
@@ -1,56 +1,40 @@
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy
+from textwrap import dedent
+
+
+def _(text, dedent_text=True):
+    if dedent_text:
+        text = dedent(text)
+    return gettext_lazy(text.strip(" \n\t\r"))
+
+
+def get_tag_translations():
+    return {
+        val: key for key, val in TRANSLATIONS["global"]["objectTags"].items()
+    }
+
+
+def translate_tag(tag, default=None):
+    return get_tag_translations().get(
+        tag, tag if default is None else default
+    )
+
 
 TRANSLATIONS = {
     "global": {
         "projectTitle": _("This was Paris in 1970"),
         "labName": _("MIT Digital Humanities Lab"),
-        "mapSquare": _("Map Square")
-    },
-    # "HomePage": {    
-    "HomePage": {
-        "wipModal": {
-            "title": _("Pardon Our Dust!"),
-            "description1": _("""
-                <strong>This was Paris in 1970</strong> is a project by
-                the <link1>MIT Digital Humanities Lab</link1> in collaboration with
-                <link2>Catherine Clark</link2>, Associate Professor of History and French Studies
-                at MIT and Director of MIT Digital Humanities.
-            """),
-            "description2": _("""
-                This project is still under construction and contains
-                student work, so there may be features that are
-                currently incomplete or inaccurate.
-            """),
-            "close": _("Close")
-        },
-        "scrollDown": _("Scroll down to enter"),
-        "explore": _("Explore Photos by Subject"),
-        "view": _("View Photos by Location"),
-        "context": _("Context and Research"),
-        "about": _("About the Project")
-    },
-    "About": {
-        "context1": _("In May 1970, thousands of amateur photographers spread out across Paris to take pictures. They were participants in a photo contest, “This was Paris in 1970,” organized by the cooperative electronics store the Fnac. Each contestant had been assigned to document a 250m square of the city. By the end of the month, this army of photographers had produced an unprecedented collection of 100,000 photographs: 70,000 black-and-white prints and 30,000 colors slides. This website currently hosts 5,000 color slides from the 13th and 19th arrondissements, areas of the city which were undergoing significant change in 1960s and 1970s."),
-        "context2": _(" The project This was Paris in 1970 provides tools to explore the rich archive: a <link1>map</link1> to see the photos square by square; an <link2>object detector</link2> to search for photos of many objects from people to cats, cars to strollers; a similar photo viewer to identify photos by composition rather than subject; and <link3>articles</link3> providing context and analysis."),
-        "team1": _("This is Paris in 1970 was created in MIT’s Digital Humanities Lab as a collaboration between DH Fellow <link1>Prof. Catherine Clark</link1>, <link2>four dozen undergraduate research associates</link2>, and <link3>the instructional staff</link3> of the DH Lab. Justice Vidal built out the first version of the site, and Nina Li spearheaded the design work."),
-        "team2": _("The <link1>Bibliothèque historique de la Ville de Paris</link1> holds the contest photographs. Its photo department made this project possible."),
-    },
-    "Explore": {
-        "rangeError": _('Desired page out of range'),
-        "paginationText1": _("Page {{currentPage}} of {{totalPages}}"),
-        "paginationText2": _("Showing Photos {{startImage}} - {{endImage}} out of {{totalCount}}"),
-        "previous": _("Previous Page"),
-        "goTo": _("Go to Page: "),
-        "next": _("Next Page"),
-        "go": _("Go"),
-        "objectsLabel": _("Objects"),
-        "objectDropdownDefault": _("All"),
+        "arrondissement": _("Arrondissement"),
+        "photosAvailable": _("Photos available"),
+        "mapSquare": _("Map Square"),
         "objectTags": {
             "person": _("person"),
             "bicycle": _("bicycle"),
             "car": _("car"),
             "motorbike": _("motorbike"),
+            "motorcycle": _("motorcycle"),
             "aeroplane": _("aeroplane"),
+            "airplane": _("airplane"),
             "bus": _("bus"),
             "train": _("train"),
             "truck": _("truck"),
@@ -105,10 +89,13 @@ TRANSLATIONS = {
             "chair": _("chair"),
             "sofa": _("sofa"),
             "pottedplant": _("pottedplant"),
+            "potted plant": _("potted plant"),
             "bed": _("bed"),
             "diningtable": _("diningtable"),
+            "dining table": _("dining table"),
             "toilet": _("toilet"),
             "tvmonitor": _("tvmonitor"),
+            "tv": _("tv"),
             "laptop": _("laptop"),
             "mouse": _("mouse"),
             "remote": _("remote"),
@@ -127,6 +114,46 @@ TRANSLATIONS = {
             "hair drier": _("hair drier"),
             "toothbrush": _("toothbrush")
         },
+    },
+    # "HomePage": {    
+    "HomePage": {
+        "wipModal": {
+            "title": _("Pardon Our Dust!"),
+            "description1": _("""
+                <strong>This was Paris in 1970</strong> is a project by
+                the <link1>MIT Digital Humanities Lab</link1> in collaboration with
+                <link2>Catherine Clark</link2>, Associate Professor of History and French Studies
+                at MIT and Director of MIT Digital Humanities.
+            """),
+            "description2": _("""
+                This project is still under construction and contains
+                student work, so there may be features that are
+                currently incomplete or inaccurate.
+            """),
+            "close": _("Close")
+        },
+        "scrollDown": _("Scroll down to enter"),
+        "explore": _("Explore Photos by Subject"),
+        "view": _("View Photos by Location"),
+        "context": _("Context and Research"),
+        "about": _("About the Project")
+    },
+    "About": {
+        "context1": _("In May 1970, thousands of amateur photographers spread out across Paris to take pictures. They were participants in a photo contest, “This was Paris in 1970,” organized by the cooperative electronics store the Fnac. Each contestant had been assigned to document a 250m square of the city. By the end of the month, this army of photographers had produced an unprecedented collection of 100,000 photographs: 70,000 black-and-white prints and 30,000 colors slides. This website currently hosts 5,000 color slides from the 13th and 19th arrondissements, areas of the city which were undergoing significant change in 1960s and 1970s."),
+        "context2": _(" The project This was Paris in 1970 provides tools to explore the rich archive: a <link1>map</link1> to see the photos square by square; an <link2>object detector</link2> to search for photos of many objects from people to cats, cars to strollers; a similar photo viewer to identify photos by composition rather than subject; and <link3>articles</link3> providing context and analysis."),
+        "team1": _("This is Paris in 1970 was created in MIT’s Digital Humanities Lab as a collaboration between DH Fellow <link1>Prof. Catherine Clark</link1>, <link2>four dozen undergraduate research associates</link2>, and <link3>the instructional staff</link3> of the DH Lab. Justice Vidal built out the first version of the site, and Nina Li spearheaded the design work."),
+        "team2": _("The <link1>Bibliothèque historique de la Ville de Paris</link1> holds the contest photographs. Its photo department made this project possible."),
+    },
+    "Explore": {
+        "rangeError": _('Desired page out of range'),
+        "paginationText1": _("Page {{currentPage}} of {{totalPages}}"),
+        "paginationText2": _("Showing Photos {{startImage}} - {{endImage}} out of {{totalCount}}"),
+        "previous": _("Previous Page"),
+        "goTo": _("Go to Page: "),
+        "next": _("Next Page"),
+        "go": _("Go"),
+        "objectsLabel": _("Objects"),
+        "objectDropdownDefault": _("All"),        
         "imagePerPageLabel": _("Images per Page"),
         "toolExplanation": _("""
             Use these tools to sort photos according to subject. The object detection tool uses
@@ -136,11 +163,8 @@ TRANSLATIONS = {
     },
     "MapPage": {        
         "photos": _("photos"),
-        "legend": {
-            "arrondissment": _("Arrondissement"),
-            "photosAvailable": _("Photos available"),
-        },
         "return": _("Return"),
+        "descriptionHeader": _("Map"),
         "description": _("""
             In order to document the entire city, and not just its most touristy or photogenic neighborhoods,
             the organizers of “This was Paris in 1970” divided up the city in 1755 squares and assigned
@@ -165,6 +189,42 @@ TRANSLATIONS = {
         "noRecord": _("No Record"),
         "photosTaken": _("{{name}} took a total of {{numPhotos}} photos for the competition."),
         "activity": _("MAP OF ACTIVITY"),
+    },
+    "Blog": {
+        "Sidebar": {
+            "posts": _("Posts")
+        },
+        "title": _("Articles"),
+        "readMore": _("Read more"),
+        "header": _("Here you will find work exploring Paris, the contest photos, and this project's tools. Some of this is student work; some is by more established researchers. If you use the photos and would like your work to be included here, please email <link1>Catherine Clark</link1>."),
+    },
+    "BlogPost": {
+        "previewNotice": _("""
+            This page is only a preview of the post and is only visible to the author and
+            site admin.
+            To make it visible to anyone, the author or a user with blog edit access must
+            click
+            "published" in the admin panel.
+        """)
+    },
+    "PhotoView": {
+        "PHOTO": _("PHOTO"),
+        "SLIDE": _("SLIDE"),
+        "similarPhotos": _("Similar Photos"),
+        "similarPhotosHelp": _("""This is what similar photos are and how we generate them."""),
+        "sortBy": _("Sort By..."),
+        "PHOTOGRAPHER": _("PHOTOGRAPHER"),
+        "TAGS": _("TAGS"),
+        "TAGSHelp": _("This is what a tag is and how we generate them."),
+        "noTags": _("No tags to display for this photo."),
+        "LOCATION": _("LOCATION"),
+        "detailsHeader": _("Photograph Details"),
+        "photoNotFound": _("Photo with id {{photoId}} is not in database.")
+    },
+    "TagView": {
+        "tagHeader": _("Photographs tagged"),
+        "numResults": _("{{numResults}} results."),
+        "pageIndicator": _("Showing page {{currentPage}} of {{totalPages}}"),
     },
     # },
     "description": _("This project is still under construction and contains student work, so there may be features that are currently incomplete or inaccurate.")
